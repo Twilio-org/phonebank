4e4ca027ad6bf7e789d100db7b380b8b4b50865b
Aiden Nguyen <anguyen@twilio.com>
2017-07-20 15:55:01 -0700

    Added knex config file and empty server-side-test.js file

---
+++ knexfile.js
@@ -0,0 +1,35 @@
+module.exports = {
+
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/phonebank_test',
+    migrations: {
+      directory: __dirname + 'server/src/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + 'server/src/db/seeds/test'
+    }
+  },
+
+  development: {
+    client: 'pg',
+    connection: process.env.PG_CONNECTION_STRING,
+    migrations: {
+      directory: __dirname + 'server/src/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + 'server/src/db/seeds/development'
+    }
+  }
+  // Will be added once there is a production DB
+  // production: {
+  //   client: 'pg',
+  //   connection: process.env.DATABASE_URL,
+  //   migrations: {
+  //     directory: __dirname + '/db/migrations'
+  //   },
+  //   seeds: {
+  //     directory: __dirname + '/db/seeds/production'
+  //   }
+  // }
+};
---
+++ test/server-side-test.js
@@ -0,0 +1,1 @@
+server-side-test.js
