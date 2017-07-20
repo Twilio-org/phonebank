e4afb4f6c150c125088210efd80aa9ce21a8f202
Aiden Nguyen <anguyen@twilio.com>
2017-07-20 13:52:12 -0700

    Server files folder restructure

--- server/app.js
+++ server/app.js
@@ -5,9 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-var sessions = require('./routes/sessions');
+var index = require('./src/routes/index');
+var users = require('./src/routes/users');
+var sessions = require('./src/routes/sessions');
 
 var app = express();
 
