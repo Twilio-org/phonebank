49be860327d71271c7f688cfa09ff27c5f33b31d
Aiden Nguyen <anguyen@twilio.com>
2017-07-24 09:50:52 -0700

    Added 2 basic database query tests for first_name and last_name

--- test/server-side-test.js
+++ test/server-side-test.js
@@ -2,7 +2,7 @@ var testdb = require('../knexfile').test;
 var knex = require('knex')(testdb);
 var request = require('request');
 var bookshelf_bcrypt = require('bookshelf-bcrypt');
-var chai = require('chai');
+var expect = require('chai').expect;
 var chaiHttp = require('chai-http');
 var Users = require('../server/src/db/controllers/users');
 
@@ -34,18 +34,27 @@ describe('Server-side tests', function() {
       console.log(('Created users table'));
     });
 
-    return new Users({
-      first_name: 'Duy',
-      last_name: 'Nguyen',
-      password_hash: 'hatch',
-      email: 'nguyenaiden@gmail.com',
-      phone_number: '+14086428264'
-    }).save();
-    
+    return (
+      new Users({
+        first_name: 'Duy',
+        last_name: 'Nguyen',
+        password_hash: 'hatch',
+        email: 'nguyenaiden@gmail.com',
+        phone_number: '+14086428264'
+      }).save() &&
+      
+      new Users({
+        first_name: 'Andi',
+        last_name: 'Oneto',
+        password_hash: 'hatch1',
+        email: 'andi@gmail.com',
+        phone_number: '+14158287474'
+      }).save()
+    )
   });
 
   afterEach(function() {
-    // bookshelf.knex.schema.dropTable('users');
+    return bookshelf.knex.schema.dropTable('users');
   });
 
   after(function() {
@@ -53,8 +62,17 @@ describe('Server-side tests', function() {
   })
 
   describe('Data insertion', function() {
-    it('should insert data into the test database', function(done) {
-      done();
+
+    it('should return correct first name given an email', function() {
+      return new Users({email: 'nguyenaiden@gmail.com'}).fetch().then(function(model) {
+        return expect(model.attributes.first_name).to.equal('Duy');
+      });
+    });
+
+    it('should return correct last name given an email', function() {
+      return new Users({email: 'andi@gmail.com'}).fetch().then(function(model) {
+        return expect(model.attributes.last_name).to.equal('Oneto');
+      });
     })
 
   })
