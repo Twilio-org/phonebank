d0a4817985bc031ddfc18bc182374a13f71f91f1
Jadzia Thomas <jadziahax@gmail.com>
2017-07-25 11:26:51 -0700

    Make server Export/Import only needed modules to/from  passportConfig.js
    Previously, passportConfig.js exported a single object containing
    multiple objects as the default export and files imported the entire
    exported object and then called the objects they needed from it.
    
    Now, passportConfig.js exports each individual object in its own named
    export and modules only import what they need and no more.

--- server/config/auth/jwtGenerator.js
+++ server/config/auth/jwtGenerator.js
@@ -1,9 +1,9 @@
 import jwt from 'jsonwebtoken';
-import passportConfig from './passportConfig';
+import { jwtOptions } from './passportConfig';
 
 export default function genToken(userId) {
   const payload = { id: userId };
-  const token = jwt.sign(payload, passportConfig.jwtOptions.secretOrKey);
+  const token = jwt.sign(payload, jwtOptions.secretOrKey);
 
   return token;
 }
