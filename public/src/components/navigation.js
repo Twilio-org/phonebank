2032338fb5694252491146109c82d7509346c113
Lupita Davila <lupita.dav@gmail.com>
2017-07-25 16:47:16 -0700

    Navigation now takes links passed in from props

--- public/src/components/navigation.js
+++ public/src/components/navigation.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 import { ButtonToolbar, DropdownButton, MenuItem } from 'react-bootstrap';
 
-class Navigation extends Component{
+class Navigation extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentLinks: props.links() // grab passed links from props
+    };
+    // Bind
+    this.renderLinks = this.renderLinks.bind(this);
+  }
+  // Render all current links
+  renderLinks() {
+    return (
+      this.state.currentLinks.map((link, i) => {
+        return <MenuItem eventKey={i + 1} key={link.title} href={link.href}>{link.title}</MenuItem>
+      })
+    );
+  }
   render() {
     return (
       <ButtonToolbar>
         <DropdownButton title="Menu" id="menu">
-          <MenuItem eventKey="1" href="/registration">Register</MenuItem>
-          <MenuItem eventKey="2" href="/login">Login</MenuItem>
+          {this.renderLinks()}
         </DropdownButton>
       </ButtonToolbar>
     );
