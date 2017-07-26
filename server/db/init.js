6f0f3dc7bcdb58d00d67ed2a1caf6d6458f3c177
Aiden Nguyen <anguyen@twilio.com>
2017-07-25 17:00:56 -0700

    Refactored server-db connection for modularity and ease of testing
    Each function within the Users controller and the Authenticate controller takes in a Model as an argument
    
    This allows us for easier testing and choosing which db to connect to

--- server/db/init.js
+++ server/db/init.js
@@ -1,32 +1,38 @@
 import knexModule from 'knex';
 import bookshelfModule from 'bookshelf';
 
-const knex = knexModule({
-  client: 'pg',
-  connection: process.env.PG_CONNECTION_STRING,
-  debug: true
-});
+// const knex = knexModule({
+//   client: 'pg',
+//   connection: process.env.PG_CONNECTION_STRING,
+//   debug: true,
+// });
 
-const bookshelf = bookshelfModule(knex);
+// const bookshelf = bookshelfModule(knex);
 
-bookshelf.knex.schema.hasTable('users').then((exist) => {
-  if (!exist) {
-    bookshelf.knex.schema.createTableIfNotExists('users', (table) => {
-      table.increments();
-      table.string('email').notNullable().unique().index();
-      table.string('first_name').notNullable();
-      table.string('last_name').notNullable();
-      table.string('phone_number').notNullable();
-      table.string('password_hash').notNullable();
-      table.boolean('is_admin').defaultTo(false);
-      table.boolean('is_banned').defaultTo(false);
-      table.boolean('is_active').defaultTo(true);
-      table.timestamp('date_created').defaultTo(knex.fn.now());
-      table.timestamp('date_updated').defaultTo(knex.fn.now());
-    }).then(() => {
-      console.log(('Created users table'));
-    });
-  }
-});
+const initializeDB = function(config, module) {
+  module(config);
+}
 
-export default bookshelf;
+const bookshelf = function(bookshelfObj) {
+  bookshelfObj.knex.schema.hasTable('users').then((exist) => {
+    if (!exist) {
+      bookshelfObj.knex.schema.createTableIfNotExists('users', (table) => {
+        table.increments();
+        table.string('email').notNullable().unique().index();
+        table.string('first_name').notNullable();
+        table.string('last_name').notNullable();
+        table.string('phone_number').notNullable();
+        table.string('password_hash').notNullable();
+        table.boolean('is_admin').defaultTo(false);
+        table.boolean('is_banned').defaultTo(false);
+        table.boolean('is_active').defaultTo(true);
+        table.timestamp('date_created').defaultTo(knex.fn.now());
+        table.timestamp('date_updated').defaultTo(knex.fn.now());
+      }).then(() => {
+        console.log(('Created users table'));
+      });
+    }
+  });
+}
+
+export default { bookshelf, initializeDB };
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,16 +1,41 @@
 import express from 'express';
 import passport from 'passport';
-import users from '../db/controllers/users';
+import { development as devconfig } from '../../knexfile';
+import knexModule from 'knex';
+import bookshelfModule from 'bookshelf';
+import bookshelfBcrypt from 'bookshelf-bcrypt';
+import User from '../db/controllers/users';
+const { initializeDB, bookshelf } = require('../db/init').default;
+const knex = knexModule(devconfig);
+const knexdb = bookshelfModule(knex);
+let usersModel = require('../db/models/users').default;
+knexdb.plugin(bookshelfBcrypt);
+usersModel = usersModel(knexdb);
+// usersModel(knexdb);
+// const usersModel = require('../db/models/users').default(db);
+
+// const db = bookshelf(knexdb).plugin(bookshelfBcrypt);
+// bookshelf = bookshelf(db);
+// const bookshelf = require('../db/init').default(devconfig);
+// console.log(bookshelf.default)
+// import usersModel from '../db/models/users';
+// import bookshelf from 'bookshelf';
+
+
+// users = users(devconfig).plugin(bookshelfBcrypt);
+
 
 const router = express.Router();
 
 router.use(passport.authenticate('jwt', { session: false }));
 
 router.get('/:id', (req, res) => {
+  console.log('meow')
   const requestedUserId = req.params.id;
-  users.getUserById({ id: requestedUserId })
-    .then((model) => {
-      res.status(200).send(model);
+
+  User.getUserById({ id: requestedUserId }, usersModel)
+    .then((user) => {
+      res.status(200).send(user);
     });
 });
 
