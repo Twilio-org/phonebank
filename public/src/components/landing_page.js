1ff8fccac3e9912d91058570defe928be74493bb
Lupita Davila <lupita.dav@gmail.com>
2017-07-26 10:45:24 -0700

    Update landing page using react-bootstrap components

--- public/src/components/landing_page.js
+++ public/src/components/landing_page.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { PageHeader } from 'react-bootstrap'
+import { PageHeader, Row, Col } from 'react-bootstrap';
+
 export default class LandingPage extends Component{
 
   render(){
   const id = 1;
     return(
-      <div>
-        <PageHeader>Phonebank</PageHeader>
-        <p className="lead">A phone banking solution powered by Twilio</p>
-        <div>
+      <Row>
+        <Col md={12}>
+          <PageHeader>Phonebank</PageHeader>
+          <p className="lead">A phone banking solution powered by Twilio</p>
           <Link to={`/account/${id}`}>View Account Details</Link>
-        </div>
-      </div>
+        </Col>
+      </Row>
     );
   };
 }
