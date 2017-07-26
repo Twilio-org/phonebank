fa924a0b7a0a04aab35c9be8053026840ac73881
Andrea Oneto <aoneto@twilio.com>
2017-07-26 11:02:15 -0700

    linted index.js
    notes: ignore error about arrow functions, this structure was taken from the react-router docs
    alos ignore error for css import, it's the bootstrap styling

--- public/src/index.js
+++ public/src/index.js
@@ -16,22 +16,46 @@ import EditAccountInfo from './components/edit_account';
 
 import { authTransition } from './actions/login';
 
-const Root = props => {
-  //isLogged in will go here, does not exist yet!
-  //login redirect if logged in is true needs to redirect to the landing page
+const Root = () => {
+  // checks if user id !==null and if authToken exists iin localStorage;
+  // will refactor to seperate concerns;
   const isLoggedIn = authTransition.bind(null, store);
-  console.log(isLoggedIn)
 
   return (
     <Provider store={store}>
       <BrowserRouter>
         <App>
           <Switch>
-            <Route path="/registration" render={() => !isLoggedIn() ? (<RegistrationForm />) : (<Redirect to="/" />)} />
-            <Route path="/login" render={() => isLoggedIn() ? (<Redirect to="/account" />) : (<LogInForm />)} />
-            <Route exact path="/account/:id/edit" render={() => isLoggedIn() ? (<EditAccountInfo />) : (<Redirect to="/login" />)} />
-            <Route path="/account" render={() => isLoggedIn() ? (<AccountPage />) : (<Redirect to="/login" />)} />
-            <Route path="/" render={() => isLoggedIn() ? (<LandingPage />) : (<Redirect to="/login" />)} />
+            <Route
+              path="/registration"
+              render={
+                () => !isLoggedIn() ? (<RegistrationForm />) : (<Redirect to="/" />)
+              }
+            />
+            <Route
+              path="/login"
+              render={
+                () => isLoggedIn() ? (<Redirect to="/account" />) : (<LogInForm />)
+              }
+            />
+            <Route
+              exact path="/account/:id/edit"
+              render={
+                () => isLoggedIn() ? (<EditAccountInfo />) : (<Redirect to="/login" />)
+              }
+            />
+            <Route
+              path="/account"
+              render={
+                () => isLoggedIn() ? (<AccountPage />) : (<Redirect to="/login" />)
+              }
+            />
+            <Route
+              path="/"
+              render={
+                () => isLoggedIn() ? (<LandingPage />) : (<Redirect to="/login" />)
+              }
+            />
           </Switch>
         </App>
       </BrowserRouter>
