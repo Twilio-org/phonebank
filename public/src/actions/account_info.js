a89d0b07bcff39fce793b122c9cb82e178fc91d0
Andrea Oneto <aoneto@twilio.com>
2017-07-26 11:27:29 -0700

    linted account_info action file
    note: ignore error about local storage and unnecessary quotation

--- public/src/actions/account_info.js
+++ public/src/actions/account_info.js
@@ -1,25 +1,27 @@
 import axios from 'axios';
 import { SET_USER_ACCOUNT_INFO } from '../reducers/account_info';
 
-export function fetchUser(id){
-  console.log(id, 'id in fetch user')
-  //refactor to use backticks
-  return dispatch => axios.get(`/users/${id}`, {
-    headers: {'Authorization': ' JWT ' + localStorage.getItem('auth_token')}
-  })
-  .then(res => {
-    const userData = res.data;
-    return dispatch(setAccountInfo(userData));
-  })
-  .catch(err => {
-    console.log('error fetching user info in account_info action fetchUserInfo: ', err);
-  })
-}
-
-export function setAccountInfo(user){
+export function setAccountInfo(user) {
   return {
     type: SET_USER_ACCOUNT_INFO,
     payload: user
-  }
+  };
 }
 
+export function fetchUser(id) {
+  // refactor to use backticks
+  return dispatch => axios.get(`/users/${id}`, {
+    headers: { 'Authorization': ` JWT ${localStorage.getItem('auth_token')}` }
+  })
+  .then((res) => {
+    const userData = res.data;
+    return dispatch(setAccountInfo(userData));
+  })
+  .catch((err) => {
+    const customError = {
+      message: `error fetching user info in account_info action fetchUserInfo: ${err}`,
+      name: 'user info post request from account_info component'
+    };
+    throw customError;
+  });
+}
