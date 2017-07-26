fa35beae1e8eb1a4a0cd3f9e8144a1868e36d82b
Aiden Nguyen <anguyen@twilio.com>
2017-07-26 14:07:16 -0700

    Refactored to import from require, add promise rejection for router.get by user's id

--- server/routes/users.js
+++ server/routes/users.js
@@ -5,12 +5,12 @@ import knexModule from 'knex';
 import bookshelfModule from 'bookshelf';
 import bookshelfBcrypt from 'bookshelf-bcrypt';
 import User from '../db/controllers/users';
-const { bookshelf } = require('../db/init').default;
+import bookshelf from '../db/init';
+import Model from '../db/models/users';
 const knex = knexModule(devconfig);
 const knexdb = bookshelfModule(knex);
-let usersModel = require('../db/models/users').default;
+const usersModel = Model(knexdb);
 knexdb.plugin(bookshelfBcrypt);
-usersModel = usersModel(knexdb);
 
 
 
@@ -19,12 +19,18 @@ const router = express.Router();
 router.use(passport.authenticate('jwt', { session: false }));
 
 router.get('/:id', (req, res) => {
-  console.log('meow')
   const requestedUserId = req.params.id;
 
   User.getUserById({ id: requestedUserId }, usersModel)
     .then((user) => {
-      res.status(200).send(user);
+      if (!user) {
+        res.status(400).send('No user found with given ID');
+      } else {
+        res.status(200).send(user);
+      }
+    })
+    .catch((err) => {
+      res.status(500).send(err);
     });
 });
 
