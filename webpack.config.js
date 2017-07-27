67b750edd510921028a24c06078c95c5644d164d
Andrea Oneto <aoneto@twilio.com>
2017-07-27 16:02:32 -0700

    playing around with Jest + Enzyme configuration

--- webpack.config.js
+++ webpack.config.js
@@ -56,6 +56,11 @@ module.exports = {
       }
     ]
   },
+  externals: {
+    'react/addons': 'react',
+    'react/lib/ExecutionEnvironment': 'react',
+    'react/lib/ReactContext': 'react'
+  },
   plugins: [
     new ExtractTextPlugin('style.css')
   ]
