a6bff0c755e60e085a845d36305a76f35b09e29b
Lupita Davila <lupita.dav@gmail.com>
2017-07-27 15:18:02 -0700

    Update navigation component to display appropriate links when logged in or not

--- public/src/components/header.jsx
+++ public/src/components/header.jsx
@@ -10,12 +10,20 @@ export default class Header extends Component {
     this.getLinks = this.getLinks.bind(this);
   }
   getLinks() {
-    // CHANGE: ideally this method would get the appropriate
     // links to pass into the navigation based on session info
-    const links = [
-      { title: 'Register', href: '/registration' },
-      { title: 'Login', href: '/login' }
-    ];
+    const currentUser = this.props.userId;
+    let links = [];
+    if (currentUser) { // user is logged in aka id present
+      links = [
+        { title: 'Account', href: `/account/${currentUser}` },
+        { title: 'Logout', href: '/logout' }
+      ];
+    } else {
+      links = [
+        { title: 'Register', href: '/registration' },
+        { title: 'Login', href: '/login' }
+      ];
+    }
     return links;
   }
   render() {
@@ -28,12 +36,10 @@ export default class Header extends Component {
             </Navbar.Brand>
           </Col>
           <Col md={4} id="navigation">
-            <Navigation links={this.getLinks}/>
+            <Navigation title={!this.props.userId ? 'Menu' : this.props.userInfo.first_name} links={this.getLinks()}/>
           </Col>
         </Row>
       </Navbar>
     );
   }
 }
-
-
