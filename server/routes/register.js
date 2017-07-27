4c5b2b525c43f33d2194c063bc71c9f6b2d9ea58
Aiden Nguyen <anguyen@twilio.com>
2017-07-27 11:08:01 -0700

    Register route refactor

--- server/routes/register.js
+++ server/routes/register.js
@@ -1,12 +1,21 @@
 import express from 'express';
-import users from '../db/controllers/users';
+import { development as devconfig } from '../../knexfile';
+import knexModule from 'knex';
+import bookshelfModule from 'bookshelf';
+import bookshelfBcrypt from 'bookshelf-bcrypt';
+import User from '../db/controllers/users';
+import Model from '../db/models/users';
+const knex = knexModule(devconfig);
+const knexdb = bookshelfModule(knex).plugin(bookshelfBcrypt);
+const usersModel = Model(knexdb);
+
 
 const router = express.Router();
 
 router.post('/', (req, res) => {
   const userParams = req.body;
 
-  users.saveNewUser(userParams)
+  User.saveNewUser(userParams, usersModel)
     .then((user) => {
       if (user) {
         res.status(201).json({ message: 'Registration Successful' });
