42b05a29d3893f01e0d3cdc42780792e6234c118
Vivek Nair <mail@vivekn.net>
2017-07-28 16:01:36 -0700

    Ignore tests from linting for now Move some redux dev-ish dependencies into proper deps Minor typos in reduxMiddleware.js

--- public/reduxMiddleware.js
+++ public/reduxMiddleware.js
@@ -3,6 +3,7 @@ import { createLogger } from 'redux-logger';
 import thunk from 'redux-thunk';
 import promise from 'redux-promise-middleware';
 
-const middleware = process.env.NODE_ENV === 'dev' ? [immutable(), createLogger(), promise(), thunk] : [ promise(), thunk];
+const middleware = process.env.NODE_ENV === 'dev' ? [immutable(), createLogger(), promise(), thunk] : [promise(), thunk];
 
 module.exports = middleware;
+
