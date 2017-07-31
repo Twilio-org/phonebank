83320641d463f28e13a27b55ba6ef17d259d367a
Aiden Nguyen <anguyen@twilio.com>
2017-07-31 11:24:55 -0700

    Readjusted import paths to match with folder restructure and changed names for better readability as well as fixed spacing issues with promises

--- test/server/server-side-test.js
+++ test/server/server-side-test.js
@@ -1,23 +1,22 @@
-import { test as testconfig } from '../knexfile';
 import knexModule from 'knex';
-import { expect, Should} from 'chai';
 import bookshelfModule from 'bookshelf';
 import bookshelfBcrypt from 'bookshelf-bcrypt';
 import bcrypt from 'bcrypt';
-import User from '../server/db/controllers/users';
-import bookshelf from '../server/db/init';
-import Model from '../server/db/models/users';
+import { expect, Should } from 'chai';
+import { test as testconfig } from '../../knexfile';
+import User from '../../server/db/services/users';
+import Model from '../../server/db/models/users';
+
 const knex = knexModule(testconfig);
 const knexdb = bookshelfModule(knex).plugin(bookshelfBcrypt);
 const usersModel = Model(knexdb);
 const should = Should();
 
-describe('Server-side tests', function() {
-
-  before(function() {
+describe('Server-side tests', () => {
+  before(() => {
     knexdb.knex.schema.hasTable('users').then((exist) => {
       if (!exist) {
-        knexdb.knex.schema.createTable('users', function(table) {
+        knexdb.knex.schema.createTable('users', (table) => {
           table.increments('id');
           table.string('email').notNullable().unique('email').index();
           table.string('first_name').notNullable();
@@ -35,11 +34,11 @@ describe('Server-side tests', function() {
           console.log(err);
         });
       }
-    })
+    });
   });
 
-  after(function() {
-     return knexdb.knex.schema.dropTable('users');
+  after(() => {
+    return knexdb.knex.schema.dropTable('users');
   });
 
   describe('Data insertion', function() {
@@ -49,7 +48,7 @@ describe('Server-side tests', function() {
         last_name: 'Doe',
         password: 'hatch',
         phone_number: '+14441114444',
-        email: 'John@gmail.com' 
+        email: 'John@gmail.com'
       };
 
       this.userSaveParams2 = {
@@ -57,53 +56,53 @@ describe('Server-side tests', function() {
         last_name: 'Doe',
         password: 'hatch1',
         phone_number: '+14441114444',
-        email: 'Jane@gmail.com' 
-      }
-    })
+        email: 'Jane@gmail.com'
+      };
+    });
 
-    it('should be able to saveNewUser (test 1)', (done) => {
+    it('should be able to save first user\'s first name and last name', (done) => {
       User.saveNewUser(this.userSaveParams1, usersModel)
         .then((user) => {
           expect(user.attributes.first_name).to.equal('John');
           expect(user.attributes.last_name).to.equal('Doe');
           done();
         }, done);
-    })
+    });
 
-    it('should hash passwords upon entry (test 1)', (done) => {
-      User.getUserByEmail({ email: 'John@gmail.com'}, usersModel)
-        .then((user) => user.attributes.password_hash)
-          .then((passwordHash) => {
-            bcrypt.compare('hatch', passwordHash, (err, match) => {
-              expect(match).to.be.true;
-              done();
-            })
-          })
-    })
+    it('should hash first user\'s password', (done) => {
+      User.getUserByEmail({ email: 'John@gmail.com' }, usersModel)
+        .then(user => user.attributes.password_hash)
+        .then((passwordHash) => {
+          bcrypt.compare('hatch', passwordHash, (err, match) => {
+            expect(match).to.be.true;
+            done();
+          });
+        });
+    });
 
-    it('should be able to saveNewUser (test 2)', (done) => {
+    it('should be able to save second user\'s first name and last name', (done) => {
       User.saveNewUser(this.userSaveParams2, usersModel)
         .then((user) => {
           expect(user.attributes.first_name).to.equal('Jane');
           expect(user.attributes.last_name).to.equal('Doe');
           done();
         }, done);
-    })
+    });
 
-    it('should hash passwords upon entry (test 2)', (done) => {
-      User.getUserByEmail({ email: 'Jane@gmail.com'}, usersModel)
-        .then((user) => user.attributes.password_hash)
-          .then((passwordHash) => {
-            bcrypt.compare('hatch1', passwordHash, (err, match) => {
-              expect(match).to.be.true;
-              done();
-            })
-          })
-    })
-  })
+    it('should hash second user\'s password', (done) => {
+      User.getUserByEmail({ email: 'Jane@gmail.com' }, usersModel)
+        .then(user => user.attributes.password_hash)
+        .then((passwordHash) => {
+          bcrypt.compare('hatch1', passwordHash, (err, match) => {
+            expect(match).to.be.true;
+            done();
+          });
+        });
+    });
+  });
 
   describe('Data retrieval', function() {
-    it('should retrieve user by email (test 1)', (done) => {
+    it('should retrieve first user\'s first name, last name, and account status by email', (done) => {
       User.getUserByEmail({ email: 'John@gmail.com'}, usersModel)
         .then((user) => {
           should.exist(user);
@@ -114,7 +113,7 @@ describe('Server-side tests', function() {
         }, done);
     });
 
-    it('should retrieve user by email (test 2)', (done) => {
+    it('should retrieve second user\'s first name, last name, and account status by email', (done) => {
       User.getUserByEmail({ email: 'Jane@gmail.com'}, usersModel)
         .then((user) => {
           should.exist(user);
@@ -125,29 +124,28 @@ describe('Server-side tests', function() {
         }, done);
     });
     
-    it ('should retrieve user by ID (test 1)', (done) => {
+    it('should retrieve first user\'s first name, last name, and account status by ID', (done) => {
       User.getUserById({ id: 1 }, usersModel)
         .then((user) => {
           should.exist(user);
           expect(user.attributes.first_name).to.equal('John');
           expect(user.attributes.last_name).to.equal('Doe');
-          expect(user.attributes.is_active).to.equal(true);
+          expect(user.attributes.is_active).to.be.true;
           done();
-        }, done)
+        }, done);
     });
 
-    it ('should retrieve user by ID (test 2)', (done) => {
+    it('should retrieve second user\'s first name, last name, and account status by ID', (done) => {
       User.getUserById({ id: 2 }, usersModel)
         .then((user) => {
           should.exist(user);
           expect(user.attributes.first_name).to.equal('Jane');
           expect(user.attributes.last_name).to.equal('Doe');
-          expect(user.attributes.is_active).to.equal(true);
+          expect(user.attributes.is_active).to.be.true;
           done();
-        }, done)
+        }, done);
     });
-  })
-
+  });
 
   describe('Data update', function() {
     beforeEach(() => {
@@ -170,64 +168,63 @@ describe('Server-side tests', function() {
       };
     });
 
-    it('should update user email by ID', (done) => {
-      User.updateUserById(this.userUpdateParams2, usersModel)
-        .then((user) => user.attributes.email)
-          .then(email => { 
-            expect(email).to.equal('Jane@yahoo.com');
-            done();
-          }, done);
-    });
 
-    it('should update user last_name by ID', (done) => {
+    it('should update first user\'s email by ID', (done) => {
       User.updateUserById(this.userUpdateParams1, usersModel)
-        .then((user) => user.attributes.last_name)
-          .then((lastName) => {
-            expect(lastName).to.equal('Wilson');
-            done();
-          }, done);
+        .then(user => user.attributes.last_name)
+        .then((lastName) => {
+          expect(lastName).to.equal('Wilson');
+          done();
+        }, done);
     });
 
-    it('should rehash passwords upon update (test 1)', (done) => {
+    it('should update second user\'s email by ID', (done) => {
+      User.updateUserById(this.userUpdateParams2, usersModel)
+        .then(user => user.attributes.email)
+        .then((email) => { 
+          expect(email).to.equal('Jane@yahoo.com');
+          done();
+        }, done);
+    });
+
+    it('should rehash first user\'s password upon update by ID', (done) => {
       User.getUserById({ id: 1 }, usersModel)
-        .then((user) => user.attributes.password_hash)
-          .then((passwordHash) => {
-            bcrypt.compare('bigowl', passwordHash, (err, match) => {
-              expect(match).to.be.true;
-              done();
-            })
-          })
-    })
+        .then(user => user.attributes.password_hash)
+        .then((passwordHash) => {
+          bcrypt.compare('bigowl', passwordHash, (err, match) => {
+            expect(match).to.be.true;
+            done();
+          });
+        });
+    });
 
-    it('should rehash passwords upon update (test 2)', (done) => {
+    it('should rehash second user\'s password upon update by ID', (done) => {
       User.getUserById({ id: 2 }, usersModel)
-        .then((user) => user.attributes.password_hash)
-          .then((passwordHash) => {
-            bcrypt.compare('smallowl', passwordHash, (err, match) => {
-              expect(match).to.be.true;
-              done();
-            })
-          }, done)
-    })
+        .then(user => user.attributes.password_hash)
+        .then((passwordHash) => {
+          bcrypt.compare('smallowl', passwordHash, (err, match) => {
+            expect(match).to.be.true;
+            done();
+          });
+        }, done);
+    });
 
-    it('should deactivate user when given an ID (test 1)', (done) => {
+    it('should deactivate first user\'s account by ID', (done) => {
       User.deactivateUserById({ id: 1 }, usersModel)
-        .then((user) => user.attributes.is_active)
-          .then((status) => {
-            expect(status).to.be.false;
-            done();
-          }, done) 
+        .then(user => user.attributes.is_active)
+        .then((status) => {
+          expect(status).to.be.false;
+          done();
+        }, done);
     });
 
-    it('should deactivate user when given an ID (test 2)', (done) => {
+    it('should deactivate second user\'s account by ID', (done) => {
       User.deactivateUserById({ id: 2 }, usersModel)
-        .then((user) => user.attributes.is_active)
-          .then((status) => {
-            expect(status).to.be.false;
-            done();
-          }, done) 
+        .then(user => user.attributes.is_active)
+        .then((status) => {
+          expect(status).to.be.false;
+          done();
+        }, done);
     });
-
   });
-})
-
+});
