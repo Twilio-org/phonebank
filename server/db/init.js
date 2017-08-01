ecfef8c9b46e54fb11356aa17223b1b15f3decc9
Aiden Nguyen <anguyen@twilio.com>
2017-08-01 16:41:49 -0700

    Add campaign database model in init.js

--- server/db/init.js
+++ server/db/init.js
@@ -26,3 +26,22 @@ bookshelf.knex.schema.hasTable('users').then((exist) => {
     });
   }
 });
+
+bookshelf.knex.schema.hasTable('campaign').then((exist) => {
+  if (!exist) {
+    bookshelf.knex.schema.createTable('campaign', (table) => {
+      table.increments();
+      table.string('name').notNullable().unique().index();
+      table.string('title').notNullable();
+      table.string('description').notNullable();
+      table.enu('status').notNullable();
+      table.integer('contact_list_id').references('contact_lists.id');
+      table.integer('script_id').references('scripts.id');
+      table.timestamp('date_created').defaultTo(bookshelf.knex.fn.now());
+      table.timestamp('date_updated').defaultTo(bookshelf.knex.fn.now());
+    }).then(() => {
+      console.log(('Created users table'));
+    });
+  }
+});
+
