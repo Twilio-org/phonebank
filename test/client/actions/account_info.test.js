a65d6f4bc91306013c5f350ef73efce9ca7449b4
Allie Glotfelty <aglotfelty@twilio.com>
2017-08-01 06:58:20 -0700

    Added nock and redux-mock-store to package.json and added action.test for fetchUser action creator.

--- test/client/actions/account_info.test.js
+++ test/client/actions/account_info.test.js
@@ -1,7 +1,31 @@
-import { setAccountInfo } from '../../../public/src/actions/account_info';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import nock from 'nock';
+import { setAccountInfo, fetchUser } from '../../../public/src/actions/account_info';
+import { SET_USER_ACCOUNT_INFO } from '../../../public/src/reducers/account_info';
 
-describe('actions', () => {
-  describe('setAccountInfo', () => {
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+
+const localStorageMock = (() => {
+  let store = {};
+  return {
+    getItem(key) {
+      return store[key];
+    },
+    setItem(key, value) {
+      store[key] = value.toString();
+    },
+    clear() {
+      store = {};
+    }
+  };
+})();
+
+global.localStorage = localStorageMock;
+
+describe('accountInfoActions', () => {
+  describe('setAccountInfoAction', () => {
     it('should have a type of "SET_USER_ACCOUNT_INFO"', () => {
       expect(setAccountInfo().type).toEqual('SET_USER_ACCOUNT_INFO');
     });
@@ -40,6 +64,74 @@ describe('actions', () => {
         phone_number: '15555555555'
       };
       expect(setAccountInfo(user).payload.phone_number).toEqual(user.phone_number);
+    });
+  });
+  describe('fetchUserAction', () => {
+    it('should execute fetchUser data', () => {
+      nock('http://localhost:3000')
+        .get('/users/1')
+        .reply(200, { body: {
+          id: 1,
+          first_name: 'Oscar',
+          last_name: 'Grouch',
+          email: 'oscar@g.com',
+          phone_number: '15555555555'
+        }
+        });
+
+      const initialState = {
+        first_name: null,
+        last_name: null,
+        email: null,
+        phone_number: null
+      };
+      const store = mockStore(initialState);
+      const user = {
+        first_name: 'Oscar',
+        last_name: 'Grouch',
+        email: 'oscar@g.com',
+        phone_number: '15555555555'
+      };
+      const expectedActions = { type: SET_USER_ACCOUNT_INFO, payload: user };
+
+      return store.dispatch(fetchUser(1))
+        .then(() => {
+          const actions = store.getActions();
+          expect(actions[0]).toEqual(expectedActions);
+        });
+    });
+    it('should add fetched data to the store', () => {
+      nock('http://localhost:3000')
+        .get('/users/1')
+        .reply(200, { body: {
+          id: 1,
+          first_name: 'Oscar',
+          last_name: 'Grouch',
+          email: 'oscar@g.com',
+          phone_number: '15555555555'
+        }
+        });
+
+      const initialState = {
+        first_name: null,
+        last_name: null,
+        email: null,
+        phone_number: null
+      };
+      const store = mockStore(initialState);
+      const user = {
+        first_name: 'Oscar',
+        last_name: 'Grouch',
+        email: 'oscar@g.com',
+        phone_number: '15555555555'
+      };
+
+      return store.dispatch(fetchUser(1))
+        .then(() => {
+          const storeState = store.getState();
+          console.log(storeState);
+          expect(storeState).toEqual(user);
+        });
     });
   });
 });
