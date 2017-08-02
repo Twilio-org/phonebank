70c98df0759d2901be6640205c6ccd85be1bb709
Andrea Oneto <aoneto@twilio.com>
2017-08-02 16:53:49 -0700

    debugging questions service testing + table not being created wip

--- test/server/services/questions.js
+++ test/server/services/questions.js
@@ -8,6 +8,7 @@ import Model from '../../../server/db/models/questions';
 const knex = knexModule(testconfig);
 const knexdb = bookshelfModule(knex);
 const questionsModel = Model(knexdb);
+
 const assert = chai.assert;
 const should = chai.Should();
 const expect = chai.expect;
@@ -17,7 +18,7 @@ describe('Question service tests', () => {
     knexdb.knex.schema.hasTable('questions').then((exist) => {
       if (!exist) {
         knexdb.knex.schema.createTable('questions', (table) => {
-          table.increments().primary();
+          table.increments('id').primary();
           table.string('title').notNullable().index();
           table.text('description').notNullable().index();
           table.enu('type', ['multiselect', 'singleselect', 'paragraph']).notNullable();
@@ -63,11 +64,15 @@ describe('Question service tests', () => {
       this.paramsArray.forEach((questionObj) => {
         Question.saveNewQuestion(questionObj, questionsModel)
           .then((question) => {
+            console.log('@@@@@@@@@@@@@@@@@@@@@@@QUESTION: ', question)
             expect(question.attributes.title).to.equal(questionObj.title);
             expect(question.attributes.description).to.equal(questionObj.description);
             expect(question.attributes.type).to.equal(questionObj.type);
             expect(question.attributes.responses).to.equal(questionObj.responses);
-          }, done);
+          }, done)
+          .catch((err) => {
+            console.log('error with save question title, description, type, and responses options', err);
+          });
       });
     });
   });
@@ -79,7 +84,10 @@ describe('Question service tests', () => {
         .then((questions) => {
           expect(questions.length).to.equal(3);
           done();
-        }, done);
+        }, done)
+        .catch((err) => {
+            console.log('error with should retrieve all questions in the table', err);
+        });
     });
 
     it('should return all questions in decending order from last update', (done) => {
@@ -87,7 +95,10 @@ describe('Question service tests', () => {
         .then((questions) => {
           expect(questions[0].attributes.title).to.equal('Testing Tables');
           expect(questions[2].attributes.title).to.equal('Still Testingggg Tablessss');
-        }, done);
+        }, done)
+        .catch((err) => {
+            console.log('error with return all questions in decending order from last update', err);
+        });
     });
 
     it('should retrieve a question by id', (done) => {
@@ -95,19 +106,28 @@ describe('Question service tests', () => {
         .then((question) => {
           expect(question.attributes.title).to.equal('Testing Tables');
           done();
-        }, done);
+        }, done)
+        .catch((err) => {
+            console.log('error with should retrieve a question by id', err);
+        });
 
       Question.getQuestionById({ id: 2 }, questionsModel)
         .then((question) => {
           expect(question.attributes.title).to.equal('Still Testing Tables');
           done();
-        }, done);
+        }, done)
+        .catch((err) => {
+            console.log('error with should retrieve a question by id', err);
+        });
 
       Question.getQuestionById({ id: 3 }, questionsModel)
         .then((question) => {
           expect(question.attributes.title).to.equal('Still Testingggg Tablessss');
           done();
-        }, done);
+        }, done)
+        .catch((err) => {
+            console.log('error with should retrieve a question by id', err);
+        });
     });
   });
 });
