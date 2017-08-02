f2f4ff733a3901eadf0549a7365b0fd1f74b53f5
Andrea Oneto <aoneto@twilio.com>
2017-08-02 11:32:48 -0700

    added routes, controllers and db services for questions also updated middleware to include questions route
    next: work with jadzia on join table between scripts and questions

--- server/db/services/questions.js
+++ server/db/services/questions.js
@@ -29,6 +29,7 @@ export default {
 
   // getQuestionByEditDate: (params, Model) => {
   //   const { updated_at } = params;
+
   //   return new Model({ updated_at }).fetch();
   // }
   // getQuestionCreatedBy: (params, Model) => {
