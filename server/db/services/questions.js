738d2ec1c9fed0fbab693d9f3e4885278fba8a0c
Andrea Oneto <aoneto@twilio.com>
2017-08-02 10:49:43 -0700

    basic db services for the questions model
    next: will work on route controllers for questions requests

---
+++ server/db/services/questions.js
@@ -0,0 +1,30 @@
+export default {
+  saveNewQuestion: (params, Model) => {
+    // TODO: will update with created_by when we establish relation to users
+    const { title, description, type, responses } = params;
+
+    return new Model({ title, description, type, responses }).save();
+  },
+  getAllQuestions: (params, Model) => {
+    return Model.forge().orderBy('updated_at', 'DESC').fetchAll();
+  },
+  getQuestionByTitle: (params, Model) => {
+    // TODO: I think title should be unique, if we decide this is not the case this will need a small refactor
+    const { title } = params;
+
+    return new Model({ title }).fetch();
+  },
+  getQuestionByCreationDate: (params, Model) => {
+    const { created_at } = params;
+
+    return new Model({ created_at }).fetch();
+  },
+  getQuestionByEditDate: (params, Model) => {
+    const { updated_at } = params;
+
+    return new Model({ updated_at }).fetch();
+  }
+  // getQuestionCreatedBy: (params, Model) => {
+    // TODO: wait for user relations
+  // }
+};
