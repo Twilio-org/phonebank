61c1f68576ac332bc8aaca07872c53bc4a64524a
Jadzia Thomas <jadziahax@gmail.com>
2017-08-02 16:59:59 -0700

    Only left self or admin modify or see user profile.
    The users controller now only serves user data or allows modification of
    user data (including deactivation) if the authorized user making the
    request matches the user the operation is being performed on OR if the
    user making the request is an admin.

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -4,7 +4,6 @@ import bookshelfBcrypt from 'bookshelf-bcrypt';
 import usersService from '../db/services/users';
 import { development as devconfig } from '../../knexfile';
 import User from '../db/models/users';
-// import bookshelfCreateTable from '../db/init';
 
 const knex = knexModule(devconfig);
 const bookshelf = bookshelfModule(knex);
@@ -28,11 +27,12 @@ export function getUserByEmail(req, res, next) {
 }
 
 export function getUserById(req, res, next) {
-  const params = {
-    id: req.params.id
-  };
+  if (req.user.attributes.is_admin === true || req.user.id === parseInt(req.params.id, 10)) {
+    const params = {
+      id: req.params.id
+    };
 
-  return usersService.getUserById(params, UsersModel)
+    return usersService.getUserById(params, UsersModel)
     .then((user) => {
       if (user) {
         res.status(200).json(user);
@@ -43,19 +43,22 @@ export function getUserById(req, res, next) {
     .catch((err) => {
       console.log('could not fetch user by id: ', err);
     });
+  }
+  return res.status(401).json({ message: 'not authorized' });
 }
 
 export function updateUserById(req, res, next) {
-  const params = {
-    id: req.params.id,
-    firstName: req.body.first_name,
-    lastName: req.body.last_name,
-    password: req.body.password,
-    phoneNumber: req.body.phone_number,
-    email: req.body.email
-  };
+  if (req.user.attributes.is_admin === true || req.user.id === parseInt(req.params.id, 10)) {
+    const params = {
+      id: req.params.id,
+      firstName: req.body.first_name,
+      lastName: req.body.last_name,
+      password: req.body.password,
+      phoneNumber: req.body.phone_number,
+      email: req.body.email
+    };
 
-  return usersService.updateUserById(params, UsersModel)
+    return usersService.updateUserById(params, UsersModel)
     .then((user) => {
       if (user) {
         res.status(200).json(user);
@@ -66,14 +69,17 @@ export function updateUserById(req, res, next) {
     .catch((err) => {
       console.log('could not update user: ', err);
     });
+  }
+  return res.status(401).json({ message: 'not authorized' });
 }
 
 export function deactivateUserById(req, res, next) {
-  const params = {
-    id: req.params.id
-  };
+  if (req.user.attributes.is_admin === true || req.user.id === parseInt(req.params.id, 10)) {
+    const params = {
+      id: req.params.id
+    };
 
-  return usersService.deactivateUserById(params, UsersModel)
+    return usersService.deactivateUserById(params, UsersModel)
     .then((user) => {
       if (user) {
         res.status(200).json(user);
@@ -84,4 +90,6 @@ export function deactivateUserById(req, res, next) {
     .catch((err) => {
       console.log('could not deactivateUser', err);
     });
+  }
+  return res.status(401).json({ message: 'not authorized' });
 }
