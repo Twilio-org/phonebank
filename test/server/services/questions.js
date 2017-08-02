731dad108d96feb88dd18b5102c8ffa86dbe08c7
Andrea Oneto <aoneto@twilio.com>
2017-08-02 16:53:49 -0700

    debugging questions service testing + table not being created wip

--- test/server/services/questions.js
+++ test/server/services/questions.js
@@ -8,10 +8,7 @@ import Model from '../../../server/db/models/questions';
 const knex = knexModule(testconfig);
 const knexdb = bookshelfModule(knex);
 const questionsModel = Model(knexdb);
-<<<<<<< HEAD
 
-=======
->>>>>>> basic outline of tests for the questions services
 const assert = chai.assert;
 const should = chai.Should();
 const expect = chai.expect;
@@ -21,11 +18,7 @@ describe('Question service tests', () => {
     knexdb.knex.schema.hasTable('questions').then((exist) => {
       if (!exist) {
         knexdb.knex.schema.createTable('questions', (table) => {
-<<<<<<< HEAD
           table.increments('id').primary();
-=======
-          table.increments().primary();
->>>>>>> basic outline of tests for the questions services
           table.string('title').notNullable().index();
           table.text('description').notNullable().index();
           table.enu('type', ['multiselect', 'singleselect', 'paragraph']).notNullable();
@@ -71,22 +64,14 @@ describe('Question service tests', () => {
       this.paramsArray.forEach((questionObj) => {
         Question.saveNewQuestion(questionObj, questionsModel)
           .then((question) => {
-<<<<<<< HEAD
-            console.log('@@@@@@@@@@@@@@@@@@@@@@@QUESTION: ', question)
-=======
->>>>>>> basic outline of tests for the questions services
             expect(question.attributes.title).to.equal(questionObj.title);
             expect(question.attributes.description).to.equal(questionObj.description);
             expect(question.attributes.type).to.equal(questionObj.type);
             expect(question.attributes.responses).to.equal(questionObj.responses);
-<<<<<<< HEAD
           }, done)
           .catch((err) => {
             console.log('error with save question title, description, type, and responses options', err);
           });
-=======
-          }, done);
->>>>>>> basic outline of tests for the questions services
       });
     });
   });
@@ -98,14 +83,10 @@ describe('Question service tests', () => {
         .then((questions) => {
           expect(questions.length).to.equal(3);
           done();
-<<<<<<< HEAD
         }, done)
         .catch((err) => {
             console.log('error with should retrieve all questions in the table', err);
         });
-=======
-        }, done);
->>>>>>> basic outline of tests for the questions services
     });
 
     it('should return all questions in decending order from last update', (done) => {
@@ -113,14 +94,10 @@ describe('Question service tests', () => {
         .then((questions) => {
           expect(questions[0].attributes.title).to.equal('Testing Tables');
           expect(questions[2].attributes.title).to.equal('Still Testingggg Tablessss');
-<<<<<<< HEAD
         }, done)
         .catch((err) => {
             console.log('error with return all questions in decending order from last update', err);
         });
-=======
-        }, done);
->>>>>>> basic outline of tests for the questions services
     });
 
     it('should retrieve a question by id', (done) => {
@@ -128,40 +105,28 @@ describe('Question service tests', () => {
         .then((question) => {
           expect(question.attributes.title).to.equal('Testing Tables');
           done();
-<<<<<<< HEAD
         }, done)
         .catch((err) => {
             console.log('error with should retrieve a question by id', err);
         });
-=======
-        }, done);
->>>>>>> basic outline of tests for the questions services
 
       Question.getQuestionById({ id: 2 }, questionsModel)
         .then((question) => {
           expect(question.attributes.title).to.equal('Still Testing Tables');
           done();
-<<<<<<< HEAD
         }, done)
         .catch((err) => {
             console.log('error with should retrieve a question by id', err);
         });
-=======
-        }, done);
->>>>>>> basic outline of tests for the questions services
 
       Question.getQuestionById({ id: 3 }, questionsModel)
         .then((question) => {
           expect(question.attributes.title).to.equal('Still Testingggg Tablessss');
           done();
-<<<<<<< HEAD
         }, done)
         .catch((err) => {
             console.log('error with should retrieve a question by id', err);
         });
-=======
-        }, done);
->>>>>>> basic outline of tests for the questions services
     });
   });
 });
