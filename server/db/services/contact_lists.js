436ccbd8f4efff1123e793851fc54f0cb8e05aa1
Lupita Davila <lupita.dav@gmail.com>
2017-08-02 15:01:42 -0700

    Completed contact list services

--- server/db/services/contact_lists.js
+++ server/db/services/contact_lists.js
@@ -1,11 +1,43 @@
 export default {
   saveNewContactList: (params, Model) => {
+    const extractedParams = {
+      name: params.name
+    };
+
+    return new Model(extractedParams).save();
   },
-  getUsersByContactList: (params, Model) => {
+  addContactToContactList: (params, Model) => {
+    const id = params.id;
+    const contactId = params.contactId;
+    return new Model({ id })
+      .contacts().attach(contactId);
+  },
+  getContactsInContactList: (params, Model) => {
+    const { id } = params;
+    return new Model({ id })
+      .contacts().fetch();
   },
   getContactListById: (params, Model) => {
+    const { id } = params;
+
+    return new Model({ id })
+      .fetch();
   },
   getContactListByName: (params, Model) => {
-  },
+    const { name } = params;
 
-}
+    return new Model({ name })
+      .fetch();
+  },
+  updateContactListById: (params, Model) => {
+    const { id } = params;
+    const extractedParams = {
+      name: params.name
+    };
+    return new Model()
+      .where({ id })
+      .save(extractedParams, {
+        method: 'update'
+      });
+  }
+};
