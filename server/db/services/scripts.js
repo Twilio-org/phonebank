f7fddf929aaeec8b6d76c33ef28fa8022fdc7bfc
Jadzia Thomas <jadziahax@gmail.com>
2017-08-02 15:39:14 -0700

    Add controller, service, and test for scripts.
    Adds the controller, services and test for scripts. Scripts can be
    created, fetched singly by id or by all, updated by id, and destroyed by
    id.

---
+++ server/db/services/scripts.js
@@ -0,0 +1,42 @@
+export default {
+  saveNewScript: (params, Model) => {
+    const extractedParams = {
+      name: params.name,
+      body: params.body,
+      description: params.description
+    };
+
+    return new Model(extractedParams).save();
+  },
+
+  getAllScripts: Model => Model.fetch(),
+
+  getScriptById: (params, Model) => {
+    const { id } = params;
+
+    return new Model({ id })
+      .fetch();
+  },
+
+  updateScriptById: (params, Model) => {
+    const { id } = params;
+    const extractedParams = {
+      name: params.name,
+      body: params.body,
+      description: params.description
+    };
+    return new Model()
+      .where({ id })
+      .save(extractedParams, {
+        method: 'update'
+      });
+  },
+
+  deleteScriptById: (params, Model) => {
+    const { id } = params;
+
+    return new Model({ id })
+      .destroy()
+      .then(model => model);
+  }
+};
