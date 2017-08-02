4252540f5314fc7d117cd39ec6246f94824a9f3b
Andrea Oneto <aoneto@twilio.com>
2017-08-02 15:54:11 -0700

    basic outline of tests for the questions services

---
+++ server/config/routes/scripts.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import { saveNewScript,
+         getAllScripts,
+         getScriptById,
+         updateScriptById,
+         addQuestionToScript,
+         getQuestionsByScriptId,
+         deleteScriptById } from '../../controllers/scripts';
+
+const router = express.Router();
+
+router.route('/').post(saveNewScript);
+router.route('/').get(getAllScripts);
+router.route('/:id').get(getScriptById);
+router.route('/:id').put(updateScriptById);
+router.route('/:id').delete(deleteScriptById);
+router.route('/addQuestionToScript/:id/:questionId/:sequenceNum').post(addQuestionToScript);
+router.route('/getQuestionsInScript/:id').get(getQuestionsByScriptId);
+
+export default router;
