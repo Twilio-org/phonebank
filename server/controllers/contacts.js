2bb4c1a1651ad6575bb3899a7a93397d2305eaf9
Allie Glotfelty <aglotfelty@twilio.com>
2017-08-03 16:30:54 -0700

    Update contact controller.

--- server/controllers/contacts.js
+++ server/controllers/contacts.js
@@ -17,7 +17,7 @@ export function saveNewContact(req, res, next) {
     external_id: req.body.external_id
   };
 
-  contactsService.saveNewContact(contactParams, ContactsModel)
+  return contactsService.saveNewContact(contactParams, ContactsModel)
     .then((contact) => {
       if (contact) {
         res.status(201).json({ message: 'Contact creation successful' });
@@ -28,6 +28,24 @@ export function saveNewContact(req, res, next) {
     .catch((err) => {
       console.log(err);
       res.status(401).json({ message: 'Contact creation unsuccessful' });
+    });
+}
+
+export function getContactById(req, res, next) {
+  const params = {
+    id: req.params.id
+  };
+
+  return contactsService.getContactByExternalId(params, ContactsModel)
+    .then((contact) => {
+      if (contact) {
+        res.status(200).json(contact);
+      } else {
+        next();
+      }
+    })
+    .catch((err) => {
+      console.log('could not fetch contact by id: ', err);
     });
 }
 
@@ -69,7 +87,7 @@ export function getContactByPhoneNumber(req, res, next) {
 
 export function updateContactById(req, res, next) {
   const params = {
-    id: req.body.id,
+    id: req.params.id,
     first_name: req.body.first_name,
     last_name: req.body.last_name,
     phone_number: req.body.phone_number,
@@ -91,10 +109,10 @@ export function updateContactById(req, res, next) {
 
 export function updateContactDoNotCallById(req, res, next) {
   const params = {
-    id: req.body.id,
-    do_not_call: req.body.do_not_call
+    id: req.params.id,
+    do_not_call: true
   };
-  return contactsService.updateContactDoNotCallById(params, ContactsModel)
+  return contactsService.updateContactById(params, ContactsModel)
     .then((contact) => {
       if (contact) {
         res.status(200).json(contact);
@@ -109,10 +127,10 @@ export function updateContactDoNotCallById(req, res, next) {
 
 export function updateContactInvalidNumberById(req, res, next) {
   const params = {
-    id: req.body.id,
-    is_invalid_number: req.body.is_invalid_number
+    id: req.params.id,
+    is_invalid_number: true
   };
-  return contactsService.updateContactInvalidNumberById(params, ContactsModel)
+  return contactsService.updateContactById(params, ContactsModel)
     .then((contact) => {
       if (contact) {
         res.status(200).json(contact);
