aeddb08369cfb103f940465920ef1cff75e31426
Allie Glotfelty <aglotfelty@twilio.com>
2017-08-03 11:42:54 -0700

    Added contact_lists controllers

---
+++ server/controllers/contact_lists.js
@@ -0,0 +1,103 @@
+import knexModule from 'knex';
+import bookshelfModule from 'bookshelf';
+import contactListsService from '../db/services/contact_lists';
+import { development as devconfig } from '../../knexfile';
+import ContactList from '../db/models/contact_lists';
+
+const knex = knexModule(devconfig);
+const bookshelf = bookshelfModule(knex);
+const ContactListsModel = ContactList(bookshelf);
+
+export function saveNewContactList(req, res, next) {
+  const contactListParams = {
+    name: req.body.name
+  };
+
+  contactListsService.saveNewContactList(contactListParams, ContactListsModel)
+    .then((contactList) => {
+      if (contactList) {
+        res.status(201).json({ message: 'Contact List creation successful' });
+      } else {
+        next();
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(401).json({ message: 'Contact List creation unsuccessful' });
+    });
+}
+
+// export function addContactToContactList(req, res, next) {
+
+// }
+
+export function getContactsInContactListById(req, res, next) {
+  const params = {
+    id: req.body.id
+  };
+  return contactListsService.getContactsInContactListById(params, ContactListsModel)
+    .then((contactList) => {
+      if (contactList) {
+        res.status(200).json(contactList);
+      } else {
+        next();
+      }
+    })
+    .catch((err) => {
+      console.log('could not get contacts in contact list by id: ', err);
+    });
+}
+
+export function getContactListById(req, res, next) {
+  const params = {
+    id: req.body.id
+  };
+
+  return contactListsService.getContactListById(params, ContactListsModel)
+    .then((contactList) => {
+      if (contactList) {
+        res.status(200).json(contactList);
+      } else {
+        next();
+      }
+    })
+    .catch((err) => {
+      console.log('could not fetch contact list by id: ', err);
+    });
+}
+
+export function getContactListByName(req, res, next) {
+  const params = {
+    name: req.body.name
+  };
+
+  return contactListsService.getContactListByName(params, ContactListsModel)
+    .then((contactList) => {
+      if (contactList) {
+        res.status(200).json(contactList);
+      } else {
+        next();
+      }
+    })
+    .catch((err) => {
+      console.log('could not fetch contact list by name: ', err);
+    });
+}
+
+export function updateContactListById(req, res, next) {
+  const params = {
+    id: req.body.id,
+    name: req.body.name
+  };
+  return contactListsService.updateContactListById(params, ContactListsModel)
+    .then((contactList) => {
+      if (contactList) {
+        res.status(200).json(contactList);
+      } else {
+        next();
+      }
+    })
+    .catch((err) => {
+      console.log('could not update contact list: ', err);
+    });
+}
