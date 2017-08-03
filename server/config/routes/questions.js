4ed63ea8113ec940dca66c277fd2342db89ae645
Andrea Oneto <aoneto@twilio.com>
2017-08-03 13:00:17 -0700

    updated route callback functions for questions to relfect name changes in the route controllers

--- server/config/routes/questions.js
+++ server/config/routes/questions.js
@@ -1,15 +1,15 @@
 import express from 'express';
 import { passport } from '../auth/local';
-import { saveNewQuestion,
-         getAllQuestions,
-         getQuestionById } from '../../controllers/questions';
+import { createNewQuestion,
+         fetchAllQuestions,
+         fetchQuestionById } from '../../controllers/questions';
 
 const router = express.Router();
 
 router.use(passport.authenticate('jwt', { session: false }));
 
-router.route('/').get(getAllQuestions);
-router.route('/:id').get(getQuestionById);
-router.route('/').post(saveNewQuestion);
+router.route('/').get(fetchAllQuestions);
+router.route('/:id').get(fetchQuestionById);
+router.route('/').post(createNewQuestion);
 
 export default router;
