c1e764496424fa5c7eb3dfc7fa91407dc271a7c1
Allie Glotfelty <aglotfelty@twilio.com>
2017-08-03 16:31:37 -0700

    Updated contact services.

--- server/db/services/contacts.js
+++ server/db/services/contacts.js
@@ -11,6 +11,13 @@ export default {
     return new Model(extractedParams).save();
   },
 
+  getContactById: (params, Model) => {
+    const { id } = params;
+
+    return new Model({ id })
+      .fetch();
+  },
+
   getContactByExternalId: (params, Model) => {
     const extractedParams = {
       external_id: params.external_id
@@ -36,31 +43,9 @@ export default {
       last_name: params.last_name,
       phone_number: params.phone_number,
       email: params.email,
-      external_id: params.external_id
-      // do_not_call: params.do_not_call,
-      // is_invalid_number: params.is_invalid_number
-    };
-    return new Model()
-      .where({ id })
-      .save(extractedParams, {
-        method: 'update'
-      });
-  },
-  updateContactDoNotCallById: (params, Model) => {
-    const { id } = params;
-    const extractedParams = {
-      do_not_call: !params.do_not_call
-    };
-    return new Model()
-      .where({ id })
-      .save(extractedParams, {
-        method: 'update'
-      });
-  },
-  updateContactInvalidNumberById: (params, Model) => {
-    const { id } = params;
-    const extractedParams = {
-      is_invalid_number: !params.is_invalid_number
+      external_id: params.external_id,
+      do_not_call: params.do_not_call,
+      is_invalid_number: params.is_invalid_number
     };
     return new Model()
       .where({ id })
@@ -68,4 +53,26 @@ export default {
         method: 'update'
       });
   }
+  // updateContactDoNotCallById: (params, Model) => {
+  //   const { id } = params;
+  //   const extractedParams = {
+  //     do_not_call: params.do_not_call
+  //   };
+  //   return new Model()
+  //     .where({ id })
+  //     .save(extractedParams, {
+  //       method: 'update'
+  //     });
+  // },
+  // updateContactInvalidNumberById: (params, Model) => {
+  //   const { id } = params;
+  //   const extractedParams = {
+  //     is_invalid_number: params.is_invalid_number
+  //   };
+  //   return new Model()
+  //     .where({ id })
+  //     .save(extractedParams, {
+  //       method: 'update'
+  //     });
+  // }
 };
