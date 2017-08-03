fa8da5d941181db11a38e9acb5440be17b1ea0c4
Aiden Nguyen <anguyen@twilio.com>
2017-08-03 14:46:29 -0700

    Add route and controller and service for getAllCampaigns

--- server/config/routes/campaigns.js
+++ server/config/routes/campaigns.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { passport } from '../auth/local';
-import { saveNewCampaign } from '../../controllers/campaigns';
+import { saveNewCampaign, getAllCampaigns } from '../../controllers/campaigns';
 
 const router = express.Router();
 
 router.use(passport.authenticate('jwt', { session: false }));
 
 router.route('/create').post(saveNewCampaign);
+router.route('/retrieve').get(getAllCampaigns);
 
 export default router;
--- server/controllers/campaigns.js
+++ server/controllers/campaigns.js
@@ -9,15 +9,34 @@ const knex = knexModule(devconfig);
 const bookshelf = bookshelfModule(knex);
 const campaignModel = Campaign(bookshelf);
 
-export function saveNewCampaign(req, res, next) {
+export function saveNewCampaign(req, res) {
   const { name, title, description, status, script_id } = req.body;
-
   return campaignsService.saveNewCampaign({ name, title, description, status, script_id }, campaignModel)
-    .then((campaign) => {
-        res.status(201).json({ message: 'Campaign successfully created' });
+    .then(() => {
+      res.status(201).json({ message: 'Campaign successfully created' });
     })
     .catch((err) => {
-      console.log(err);
-      res.status(401).json({ message: 'Campaign creation unsuccessful' });
+      res.status(401).json({
+        message: 'Campaign creation unsuccessful',
+        error: err
+      });
     });
 }
+
+export function getAllCampaigns(req, res) {
+  return campaignsService.getAllCampaigns(null, campaignModel)
+    .then((campaigns) => {
+      if (campaigns) {
+        res.status(200).json(campaigns);
+      } else {
+        res.status(200).json({ message: 'No campaigns found' });
+      }
+    })
+    .catch((err) => {
+      res.status(401).json({
+        message: 'Cannot retrieve campaigns',
+        error: err
+      });
+    });
+}
+
