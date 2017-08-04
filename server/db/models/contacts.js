b6fbd0add5f3cd1a340d30ac164108fe5783c4d0
Allie Glotfelty <aglotfelty@twilio.com>
2017-08-04 15:38:12 -0700

    Updated for join with contact_lists table

--- server/db/models/contacts.js
+++ server/db/models/contacts.js
@@ -1,11 +1,17 @@
+import knexModule from 'knex';
+import bookshelfModule from 'bookshelf';
 import ContactList from './contact_lists';
+import { development as devconfig } from '../../../knexfile';
+
+const knex = knexModule(devconfig);
+const bookshelf = bookshelfModule(knex);
 
 export default function Contact(bookshelfObj) {
   return bookshelfObj.Model.extend({
     tableName: 'contacts',
     hasTimestamps: true,
     contact_lists() {
-      return this.belongsToMany(ContactList);
+      return this.belongsToMany(ContactList(bookshelf));
     }
   });
 }
