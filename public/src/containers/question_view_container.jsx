451e811084cc89f6695632c40831440ed4bd0bf9
Lupita Davila <lupita.dav@gmail.com>
2017-08-08 16:30:31 -0700

    Create question view and form, link it all

--- public/src/containers/question_view_container.jsx
+++ public/src/containers/question_view_container.jsx
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+import QuestionView from '../components/question_view';
 
 class QuestionViewContainer extends Component {
   render() {
-    return 'QuestionViewContainer';
+    const props = {}; // should be grabbing from store via connect
+    return (
+      <QuestionView {...props} />
+    );
   }
 }
 function mapStateToProps(state) {
-  return {};
+  return { state };
 }
 export default withRouter(
   connect(mapStateToProps, null)(QuestionViewContainer)
