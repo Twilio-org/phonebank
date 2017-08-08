e5a62b347cceb31438acdd94da155eb9f5e6ea63
Lupita Davila <lupita.dav@gmail.com>
2017-08-07 17:19:20 -0700

    Create route for GET /contactLists

--- server/config/routes/contact_lists.js
+++ server/config/routes/contact_lists.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import { saveNewContactList,
          updateContactListById,
+         getAllContactLists,
          getContactListById,
          addContactToContactList } from '../../controllers/contact_lists';
 // add updateContactInvalidNumberById
 const router = express.Router();
 
+router.route('/').get(getAllContactLists);
 router.route('/').post(saveNewContactList);
 router.route('/:id').get(getContactListById);
 router.route('/:id').put(updateContactListById);
