9618b22186c1f5f5205312e4032d6cabf8b82760
Aiden Nguyen <anguyen@twilio.com>
2017-08-08 23:28:11 -0700

    Add basic skeleton of campaign component

--- public/src/components/campaign.jsx
+++ public/src/components/campaign.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Field, reduxForm } from ' redux-form';
+import { Field, reduxForm } from 'redux-form';
 import { connect } from 'react-redux';
 import { Link, withRouter } from 'react-router-dom';
 import { Button, ButtonToolbar } from 'react-bootstrap';
