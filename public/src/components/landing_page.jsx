fd7c1e64d00417ea44f2b96ecea53cc1362571fb
Aiden Nguyen <anguyen@twilio.com>
2017-08-10 14:10:02 -0700

    Removed link to add campaign on landing page Will add back on front-end PR

--- public/src/components/landing_page.jsx
+++ public/src/components/landing_page.jsx
@@ -13,7 +13,6 @@ class LandingPage extends Component {
           <PageHeader>Phonebank</PageHeader>
           <p className="lead">A phone banking solution powered by Twilio</p>
           <Link to={`/account/${id}`}>View Account Details</Link><br />
-          <Link to="/campaign">Add Campaign</Link>
         </Col>
       </Row>
     );
