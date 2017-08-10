4a394fb871c72f9ce0bc71b35d83fd492959d119
Aiden Nguyen <anguyen@twilio.com>
2017-08-10 14:29:04 -0700

    Add add campaign actions(status, scriptid, and contact_list_id still hard coded) and views

---
+++ public/src/actions/campaign.jsx
@@ -0,0 +1,44 @@
+import axios from 'axios';
+
+export default function saveNewCampaign(campaignInfo, history) {
+  const { name, title, description, status, script_id } = campaignInfo;
+
+  return dispatch => axios.post('/campaigns',
+    {
+      name: name,
+      title: title,
+      description: description,
+      status: 'draft',
+      script_id: 1,
+      contact_lists_id: 1
+    },
+    { headers: { Authorization: ` JWT ${localStorage.getItem('auth_token')}` } }
+  )
+  .then(() => {
+    history.push(`/campaign`);
+  })
+  .catch((err) => {
+    const customError = {
+      message: `error in saving campaign into database: ${err}`,
+      name: 'campaign data post request to campaign component'
+    };
+    throw customError;
+  });
+} 
+
+export function getAllCampaigns(history) {
+  return dispatch => axios.get('/campaigns',
+  null,
+  { headers: { 'Authorization': ` JWT ${localStorage.getItem('auth_token')}` } }
+  )
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    const customError = {
+      message: `error fetching campaign information: ${err}`,
+      name: 'campaign get request from the campaign component'
+    };
+    throw customError;
+  });
+}
---
+++ public/src/components/campaign.jsx
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { Button, ButtonToolbar } from 'react-bootstrap';
+import renderField from '../helpers/formHelpers';
+import saveNewCampaign from '../actions/campaign';
+
+class CampaignPage extends Component {
+  constructor(props) {
+    super(props);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onSubmit(values) {
+    const { history } = this.props;
+    this.props.saveNewCampaign(values, history);
+  }
+
+  render() {
+    const { handleSubmit } = this.props;
+    return (
+      <form onSubmit={handleSubmit(this.onSubmit)}>
+        <Field
+          name="name"
+          label="Name (internal)"
+          component={renderField}
+        />
+        <Field
+          name="title"
+          label="Title (public)"
+          component={renderField}
+        />
+        <Field
+          name="description"
+          label="Description"
+          component={renderField}
+        />
+        <ButtonToolbar>
+          <Button bsStyle="primary" type="submit">Save Campaign</Button>
+        </ButtonToolbar>
+      </form>
+    );
+  }
+}
+
+
+function validate(values) {
+  const errors = {};
+  const { password, password_confirm } = values;
+  if (!!password && !!password_confirm && password !== password_confirm) {
+    errors.password_confirm = 'the passwords must match.';
+  }
+  return errors;
+}
+
+function mapStateToProps(state) {
+  return { auth: state.auth };
+}
+
+export default withRouter(
+  reduxForm({
+    validate,
+    form: 'CampaignPage'
+  })(
+    connect(mapStateToProps, { saveNewCampaign })(CampaignPage)
+  )
+);
