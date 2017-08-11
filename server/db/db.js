6fe3e0b7efae449a0215a476d9b1007f9d562e1d
Vivek Nair <mail@vivekn.net>
2017-08-11 15:39:18 -0700

    Refactored and fixed testing

--- server/db/db.js
+++ server/db/db.js
@@ -11,136 +11,169 @@ const knex = knexModule(config);
 const bookshelf = bookshelfModule(knex);
 bookshelf.plugin(bookshelfBcrypt);
 
-bookshelf.knex.schema.hasTable('users').then((exist) => {
-  if (!exist) {
-    bookshelf.knex.schema.createTable('users', (table) => {
-      table.increments('id').primary();
-      table.string('email').notNullable().unique().index();
-      table.string('first_name').notNullable();
-      table.string('last_name').notNullable();
-      table.string('phone_number').notNullable();
-      table.string('password_hash').notNullable();
-      table.boolean('is_admin').defaultTo(false);
-      table.boolean('is_banned').defaultTo(false);
-      table.boolean('is_active').defaultTo(true);
-      table.timestamp('created_at').defaultTo(bookshelf.knex.fn.now());
-      table.timestamp('updated_at').defaultTo(bookshelf.knex.fn.now());
-    }).then(() => {
-      console.log('Created users table');
-    }).catch(err => console.log('Error creating users table', err));
-  }
-});
+const createContactsTable = () =>
+  bookshelf.knex.schema.hasTable('contacts').then((exist) => {
+    if (!exist) {
+      return bookshelf.knex.schema.createTable('contacts', (table) => {
+        table.increments('id').primary();
+        table.string('external_id').nullable();
+        table.string('first_name').notNullable();
+        table.string('last_name').nullable();
+        table.string('email').nullable();
+        table.string('phone_number').notNullable().unique().index();
+        table.boolean('is_invalid_number').defaultTo(false);
+        table.boolean('do_not_call').defaultTo(false);
+        table.timestamp('created_at').defaultTo(bookshelf.knex.fn.now());
+        table.timestamp('updated_at').defaultTo(bookshelf.knex.fn.now());
+      });
+    }
+    return exist;
+  });
 
-bookshelf.knex.schema.hasTable('contacts').then((exist) => {
-  if (!exist) {
-    bookshelf.knex.schema.createTable('contacts', (table) => {
-      table.increments('id').primary();
-      table.string('external_id').nullable();
-      table.string('first_name').notNullable();
-      table.string('last_name').nullable();
-      table.string('email').nullable();
-      table.string('phone_number').notNullable().unique().index();
-      table.boolean('is_invalid_number').defaultTo(false);
-      table.boolean('do_not_call').defaultTo(false);
-      table.timestamp('created_at').defaultTo(bookshelf.knex.fn.now());
-      table.timestamp('updated_at').defaultTo(bookshelf.knex.fn.now());
-    }).then(() => {
-      console.log(('Created contacts table'));
-    }).catch(err => console.log('Error creating contacts table', err));
-  }
-});
+const createContactListsTable = () =>
+  bookshelf.knex.schema.hasTable('contact_lists').then((exist) => {
+    if (!exist) {
+      return bookshelf.knex.schema.createTable('contact_lists', (table) => {
+        table.increments('id').primary();
+        table.string('name').notNullable().unique();
+        table.timestamp('created_at').defaultTo(bookshelf.knex.fn.now());
+        table.timestamp('updated_at').defaultTo(bookshelf.knex.fn.now());
+      });
+    }
+    return exist;
+  });
 
-bookshelf.knex.schema.hasTable('contact_lists').then((exist) => {
-  if (!exist) {
-    bookshelf.knex.schema.createTable('contact_lists', (table) => {
-      table.increments('id').primary();
-      table.string('name').notNullable().unique();
-      table.timestamp('created_at').defaultTo(bookshelf.knex.fn.now());
-      table.timestamp('updated_at').defaultTo(bookshelf.knex.fn.now());
-    }).then(() => {
-      console.log(('Created contact_lists table'));
-    }).catch(err => console.log('Error creating contact_lists table', err));
-  }
-});
+const createContactsContactListsTable = () =>
+  bookshelf.knex.schema.hasTable('contacts_contact_lists').then((exist) => {
+    if (!exist) {
+      return bookshelf.knex.schema.createTable('contacts_contact_lists', (table) => {
+        table.increments('id').primary();
+        table.integer('contact_id').references('contacts.id').notNullable();
+        table.integer('contact_list_id').references('contact_lists.id').notNullable();
+        table.unique(['contact_id', 'contact_list_id']);
+        table.timestamp('created_at').defaultTo(bookshelf.knex.fn.now());
+        table.timestamp('updated_at').defaultTo(bookshelf.knex.fn.now());
+      });
+    }
+    return exist;
+  });
 
-bookshelf.knex.schema.hasTable('contacts_contact_lists').then((exist) => {
-  if (!exist) {
-    bookshelf.knex.schema.createTable('contacts_contact_lists', (table) => {
-      table.increments('id').primary();
-      table.integer('contact_id').references('contacts.id').notNullable();
-      table.integer('contact_list_id').references('contact_lists.id').notNullable();
-      table.unique(['contact_id', 'contact_list_id']);
-      table.timestamp('created_at').defaultTo(bookshelf.knex.fn.now());
-      table.timestamp('updated_at').defaultTo(bookshelf.knex.fn.now());
-    }).then(() => {
-      console.log(('Created contacts_contact_lists table'));
-    }).catch(err => console.log('Error creating contacts_to_contact_lists table', err));
-  }
-});
+const createScriptsTable = () =>
+  bookshelf.knex.schema.hasTable('scripts').then((exist) => {
+    if (!exist) {
+      return bookshelf.knex.schema.createTable('scripts', (table) => {
+        table.increments('id').primary();
+        table.string('name').notNullable().unique().index();
+        table.text('body').notNullable();
+        table.text('description').notNullable();
+        table.timestamp('created_at').defaultTo(bookshelf.knex.fn.now());
+        table.timestamp('updated_at').defaultTo(bookshelf.knex.fn.now());
+      });
+    }
+    return exist;
+  });
 
-bookshelf.knex.schema.hasTable('scripts').then((exist) => {
-  if (!exist) {
-    bookshelf.knex.schema.createTable('scripts', (table) => {
-      table.increments('id').primary();
-      table.string('name').notNullable().unique().index();
-      table.text('body').notNullable();
-      table.text('description').notNullable();
-      table.timestamp('created_at').defaultTo(bookshelf.knex.fn.now());
-      table.timestamp('updated_at').defaultTo(bookshelf.knex.fn.now());
-    }).then(() => {
-      console.log('Created scripts table');
-    }).catch(err => console.log('Error creating scripts table', err));
-  }
-});
+const createQuestionsTable = () =>
+  bookshelf.knex.schema.hasTable('questions').then((exist) => {
+    if (!exist) {
+      return bookshelf.knex.schema.createTable('questions', (table) => {
+        table.increments('id').primary();
+        table.string('title').notNullable().unique().index();
+        table.text('description').notNullable().index();
+        table.enu('type', ['multiselect', 'singleselect', 'paragraph']).notNullable();
+        table.text('responses').notNullable();
+        table.timestamp('created_at').defaultTo(bookshelf.knex.fn.now());
+        table.timestamp('updated_at').defaultTo(bookshelf.knex.fn.now());
+      });
+    }
+    return exist;
+  });
 
-bookshelf.knex.schema.hasTable('questions').then((exist) => {
-  if (!exist) {
-    bookshelf.knex.schema.createTable('questions', (table) => {
-      table.increments('id').primary();
-      table.string('title').notNullable().unique().index();
-      table.text('description').notNullable().index();
-      table.enu('type', ['multiselect', 'singleselect', 'paragraph']).notNullable();
-      table.text('responses').notNullable();
-      table.timestamp('created_at').defaultTo(bookshelf.knex.fn.now());
-      table.timestamp('updated_at').defaultTo(bookshelf.knex.fn.now());
-    }).then(() => {
-      console.log('Created questions table');
-    }).catch(err => console.log('Error creating questions table', err));
-  }
-});
+const createQuestionsScriptsTable = () =>
+  bookshelf.knex.schema.hasTable('questions_scripts').then((exist) => {
+    if (!exist) {
+      return bookshelf.knex.schema.createTable('questions_scripts', (table) => {
+        table.increments('id').primary();
+        table.integer('script_id').references('scripts.id').notNullable();
+        table.integer('question_id').references('questions.id').notNullable();
+        table.integer('sequence_number').notNullable();
+        table.timestamp('created_at').defaultTo(bookshelf.knex.fn.now());
+        table.timestamp('updated_at').defaultTo(bookshelf.knex.fn.now());
+        table.unique(['script_id', 'question_id']);
+      });
+    }
+    return exist;
+  });
 
-bookshelf.knex.schema.hasTable('questions_scripts').then((exist) => {
-  if (!exist) {
-    bookshelf.knex.schema.createTable('questions_scripts', (table) => {
-      table.increments('id').primary();
-      table.integer('script_id').references('scripts.id').notNullable();
-      table.integer('question_id').references('questions.id').notNullable();
-      table.integer('sequence_number').notNullable();
-      table.timestamp('created_at').defaultTo(bookshelf.knex.fn.now());
-      table.timestamp('updated_at').defaultTo(bookshelf.knex.fn.now());
-      table.unique(['script_id', 'question_id']);
-    }).then(() => {
-      console.log('Created questions_scripts table');
-    }).catch(err => console.log('Error creating questions_scripts table', err));
-  }
-});
+const createCampaignsTable = () =>
+  bookshelf.knex.schema.hasTable('campaigns').then((exist) => {
+    if (!exist) {
+      return bookshelf.knex.schema.createTable('campaigns', (table) => {
+        table.increments('id').primary();
+        table.string('name').notNullable().unique().index();
+        table.string('title').notNullable();
+        table.string('description').notNullable();
+        table.enu('status', ['draft', 'active', 'pause', 'completed']).notNullable();
+        table.integer('contact_lists_id').references('contact_lists.id').notNullable();
+        table.integer('script_id').references('scripts.id').notNullable();
+        table.timestamp('created_at').defaultTo(bookshelf.knex.fn.now());
+        table.timestamp('updated_at').defaultTo(bookshelf.knex.fn.now());
+      });
+    }
+    return exist;
+  });
 
-bookshelf.knex.schema.hasTable('campaigns').then((exist) => {
-  if (!exist) {
-    bookshelf.knex.schema.createTable('campaigns', (table) => {
-      table.increments('id').primary();
-      table.string('name').notNullable().unique().index();
-      table.string('title').notNullable();
-      table.string('description').notNullable();
-      table.enu('status', ['draft', 'active', 'pause', 'completed']).notNullable();
-      table.integer('contact_lists_id').references('contact_lists.id').notNullable();
-      table.integer('script_id').references('scripts.id').notNullable();
-      table.timestamp('created_at').defaultTo(bookshelf.knex.fn.now());
-      table.timestamp('updated_at').defaultTo(bookshelf.knex.fn.now());
-    }).then(() => {
-      console.log(('Created campaigns table'));
-    }).catch(err => console.log('Error creating campaigns table', err));
-  }
-});
+const createUsersTable = () =>
+  bookshelf.knex.schema.hasTable('users').then((exist) => {
+    if (!exist) {
+      return bookshelf.knex.schema.createTable('users', (table) => {
+        table.increments('id').primary();
+        table.string('email').notNullable().unique().index();
+        table.string('first_name').notNullable();
+        table.string('last_name').notNullable();
+        table.string('phone_number').notNullable();
+        table.string('password_hash').notNullable();
+        table.boolean('is_admin').defaultTo(false);
+        table.boolean('is_banned').defaultTo(false);
+        table.boolean('is_active').defaultTo(true);
+        table.timestamp('created_at').defaultTo(bookshelf.knex.fn.now());
+        table.timestamp('updated_at').defaultTo(bookshelf.knex.fn.now());
+      });
+    }
+    return exist;
+  });
+
+const setup = () =>
+  createContactsTable()
+  .then(createContactListsTable)
+  .then(createContactsContactListsTable)
+  .then(createScriptsTable)
+  .then(createQuestionsTable)
+  .then(createQuestionsScriptsTable)
+  .then(createCampaignsTable)
+  .then(createUsersTable)
+  .then(() => {
+    console.log('Created all tables');
+  })
+  .catch((err) => {
+    console.error('Error creating tables: ', err);
+  });
+
+const teardown = () =>
+  bookshelf.knex.schema.dropTable('campaigns')
+  .then(() => bookshelf.knex.schema.dropTable('questions_scripts'))
+  .then(() => bookshelf.knex.schema.dropTable('questions'))
+  .then(() => bookshelf.knex.schema.dropTable('scripts'))
+  .then(() => bookshelf.knex.schema.dropTable('contacts_contact_lists'))
+  .then(() => bookshelf.knex.schema.dropTable('contact_lists'))
+  .then(() => bookshelf.knex.schema.dropTable('contacts'))
+  .then(() => bookshelf.knex.schema.dropTable('users'))
+  .then(() => {
+    console.log('Dropped all tables');
+  })
+  .catch((err) => {
+    console.error('Error dropping tables', err);
+  });
 
 export default bookshelf;
+export { bookshelf, setup, teardown };
--- test/server/services/contact_lists.js
+++ test/server/services/contact_lists.js
@@ -1,45 +1,11 @@
-import knexModule from 'knex';
-import bookshelfModule from 'bookshelf';
 import { expect, Should } from 'chai';
-import { test as testconfig } from '../../../knexfile';
 import ContactList from '../../../server/db/services/contact_lists';
-import Model from '../../../server/db/models/contact_lists';
 
-const knex = knexModule(testconfig);
-const bookshelf = bookshelfModule(knex);
-const contactListsModel = Model(bookshelf);
 const should = Should();
 
 describe('Contact List Service tests', () => {
-  before((done) => {
-    bookshelf.knex.schema.hasTable('contact_lists').then((exist) => {
-      if (!exist) {
-        bookshelf.knex.schema.createTable('contact_lists', (table) => {
-          table.increments('id').primary();
-          table.string('name').notNullable().unique();
-          table.timestamp('created_at').defaultTo(bookshelf.knex.fn.now());
-          table.timestamp('updated_at').defaultTo(bookshelf.knex.fn.now());
-        }).then(() => {
-          console.log(('** created contact_lists table'));
-          done();
-        }).catch((err) => {
-          console.log('! error creating contacts_lists table', err);
-        });
-      }
-    });
-  });
-  after(() =>
-    bookshelf.knex.schema.dropTable('contact_lists')
-      .then(() => {
-        console.log('* dropped contact_lists table');
-      })
-      .catch((err) => {
-        console.log('! error dropping contact_lists table', err);
-      })
-  );
-
-  describe('Data insertion', function() {
-    beforeEach(() => {
+  describe('insert/read/update', function() {
+    before(() => {
       this.params = {
         name: 'Millbrae Phone List'
       };
@@ -49,29 +15,25 @@ describe('Contact List Service tests', () => {
     });
 
     it('should create contact list and save all parameters', (done) => {
-      ContactList.saveNewContactList(this.params, contactListsModel)
+      ContactList.saveNewContactList(this.params)
         .then((contactList) => {
+          this.params.id = contactList.attributes.id;
           expect(contactList.attributes.name).to.equal(this.params.name);
           done();
         }).catch((err) => {
           console.log('error with saving contact list test', err);
         });
     });
-  });
-  describe('Data update', function() {
-    beforeEach(() => {
-      this.params = {
-        name: 'San Francisco Mission Phone List'
-      };
-    });
 
     it('should update a contact list name', (done) => {
-      ContactList.saveNewContactList(this.params, contactListsModel)
+      ContactList.saveNewContactList(this.params2)
         .then((contactList) => {
           const newParams = { id: contactList.id, name: 'New Name' };
-          ContactList.updateContactListById(newParams, contactListsModel)
+          this.params2.id = contactList.id;
+          ContactList.updateContactListById(newParams)
             .then((updatedContactList) => {
               expect(updatedContactList.attributes.name).to.equal(newParams.name);
+              this.params2.name = newParams.name;
             })
             .catch((err) => {
               console.log('error with updating contact list \n\n');
@@ -84,18 +46,13 @@ describe('Contact List Service tests', () => {
             done(err);
         });
     });
-  });
 
-  describe('Data retrieval', function() {
-    beforeEach(() => {
-      this.params = {
-        id: 1
-      };
-    });
-    it('should retrieva a contact list by name', (done) => {
-      ContactList.getContactListById(this.params, contactListsModel)
+    it('should retrieva a contact list by id', (done) => {
+      const queryParams = { id: this.params2.id };
+      ContactList.getContactListById(queryParams)
         .then((contactList) => {
-          expect(contactList.attributes.id).to.equal(1);
+          expect(contactList.attributes.id).to.equal(this.params2.id);
+          expect(contactList.attributes.name).to.equal(this.params2.name);
           done();
         })
         .catch((err) => {
@@ -103,8 +60,9 @@ describe('Contact List Service tests', () => {
             done(err);
         });
     });
+
     it('should retrieve all contact lists', (done) => {
-      ContactList.getAllContactLists(contactListsModel)
+      ContactList.getAllContactLists()
         .then((contactLists) => {
           expect(contactLists.length).to.equal(2);
           done();
