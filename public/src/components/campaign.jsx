ac9ee27c3bd83d1e4ed9b1c7999e76d4f42b1070
Aiden Nguyen <anguyen@twilio.com>
2017-08-14 14:45:51 -0700

    changed destroyOnUnmount to false to preserve the state on campaign page Form Validation WIP

--- public/src/components/campaign.jsx
+++ public/src/components/campaign.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-import { Button, ButtonToolbar, PageHeader } from 'react-bootstrap';
+import { Button, ButtonToolbar, PageHeader, Row, Col } from 'react-bootstrap';
 import { renderField, renderDropdown, renderTextArea } from '../helpers/formHelpers';
 import { saveNewCampaign } from '../actions/campaign';
 import { fetchAllScripts, fetchAllContactLists } from '../actions/campaign_form';
@@ -11,6 +11,7 @@ class CampaignPage extends Component {
   constructor(props) {
     super(props);
     this.onSubmit = this.onSubmit.bind(this);
+    this.handleCreateScript = this.handleCreateScript.bind(this);
   }
 
 
@@ -23,6 +24,10 @@ class CampaignPage extends Component {
   onSubmit(values) {
     const { history } = this.props;
     this.props.saveNewCampaign(values, history);
+  }
+
+  handleCreateScript() {
+    this.props.history.push('/admin/scripts/new');
   }
 
   render() {
@@ -41,25 +46,35 @@ class CampaignPage extends Component {
             label="Title (public)"
             component={renderField}
           />
-          <Field
-            name="description"
-            label="Description"
-            component={renderTextArea}
-          />
-          {this.props.scriptOptions ? (<Field
-            name="script_id"
-            label="Script"
-            keyToUse="name"
-            component={renderDropdown}
-            options={this.props.scriptOptions}
-          />) : null }
-          {this.props.contactListOptions ? (<Field
-            name="contact_lists_id"
-            label="Contact List"
-            keyToUse="name"
-            component={renderDropdown}
-            options={this.props.contactListOptions}
-          />) : null }
+          <Row>
+            <Col xs={5}>
+              <Field
+                name="description"
+                label="Description"
+                component={renderTextArea}
+              />
+              {this.props.scriptOptions ? (<Field
+                name="script_id"
+                label="Script"
+                keyToUse="name"
+                component={renderDropdown}
+                options={this.props.scriptOptions}
+              />) : null }
+              <Button
+                bsStyle="success"
+                onClick={this.handleCreateScript}
+              >
+                Add Script
+              </Button>
+              {this.props.contactListOptions ? (<Field
+                name="contact_lists_id"
+                label="Contact List"
+                keyToUse="name"
+                component={renderDropdown}
+                options={this.props.contactListOptions}
+              />) : null }
+            </Col>
+          </Row>
           <ButtonToolbar>
             <Button bsStyle="primary" type="submit">Save Campaign</Button>
           </ButtonToolbar>
@@ -90,7 +105,8 @@ function mapStateToProps(state) {
 export default withRouter(
   reduxForm({
     validate,
-    form: 'CampaignPage'
+    form: 'CampaignPage',
+    destroyOnUnmount: false
   })(
     connect(mapStateToProps, {
       saveNewCampaign,
