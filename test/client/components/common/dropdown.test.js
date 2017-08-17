083fb0d0364f5c4dd558fc98bb96bc297a0888f4
Lupita Davila <lupita.dav@gmail.com>
2017-08-17 16:19:58 -0700

    Component tests for common form components

---
+++ test/client/components/common/dropdown.test.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Dropdown from '../../../../public/src/components/common/form/dropdown';
+
+describe('<Dropdown />', () => {
+  const props = {
+    label: 'Do you have pets?',
+    helpText: 'e.x dog, cats, fish, birds, hamsters, bunnies',
+    name: 'pets',
+    options: [
+      {value: 'yes', label: 'Yes'},
+      {value: 'no', label: 'No'},
+      {value: 'maybe', label: 'Maybe'}
+    ],
+    meta: {
+      touched: false
+    }
+  };
+  const touchedErrorProps = {
+    meta: {
+      touched: true,
+      error: 'An error message!'
+    }
+  };
+  const metaProps = {
+    meta: {
+      touched: false
+    }
+  };
+  const wrapper1 = shallow(<Dropdown {...props} />);
+  const wrapper2 = shallow(<Dropdown {...props} {...touchedErrorProps} />);
+  const wrapper3 = shallow(<Dropdown {...metaProps} />);
+
+  describe('rendering', () => {
+    describe('bootstrap components', () => {
+      it('should render <FormGroup />', () => {
+        expect(wrapper1.find('FormGroup').length).toBe(1);
+      });
+      it('should render <ControlLabel>', () => {
+        expect(wrapper1.find('ControlLabel').length).toBe(1);
+      });
+      it('should render <FormControl>', () => {
+        expect(wrapper1.find('FormControl').length).toBe(1);
+      });
+      it('should render <HelpBlock>', () => {
+        expect(wrapper1.find('HelpBlock').length).toBe(1);
+      });
+      it('should render helpText in <HelpBlock>', () => {
+        expect(wrapper1.find('HelpBlock').html()).toContain(props.helpText);
+      });
+      it('should not render <Popover> if not touched or errors present', () => {
+        expect(wrapper1.find('Popover').length).toBe(0);
+      });
+      it('should render <Popover> if touched and errors present', () => {
+        expect(wrapper2.find('Popover').length).toBe(1);
+      });
+      it('should render errorText in <Popover>', () => {
+        expect(wrapper2.find('Popover').html()).toContain(touchedErrorProps.meta.error);
+      });
+    });
+  });
+  describe('props', () => {
+    it('should not load props if no props', () => {
+      expect(wrapper1.html()).not.toBe(wrapper3.html());
+    });
+  });
+  describe('renderOptions()', () => {
+    it('should render options', () => {
+      expect(wrapper1.html()).toContain(props.options[0].label);
+      expect(wrapper1.html()).toContain(props.options[1].label);
+      expect(wrapper1.html()).toContain(props.options[2].label);
+    });
+    it('should render <option>\'s + 1 for "select"', () => {
+      expect(wrapper1.find('option').length).toBe(props.options.length + 1);
+    });
+  });
+});
---
+++ test/client/components/common/field_group.test.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import FieldGroup from '../../../../public/src/components/common/form/field_group';
+
+describe('<FieldGroup />', () => {
+  const props = {
+    label: 'Full Name',
+    helpText: 'What is your full name?',
+    name: 'full_name',
+    type: 'text',
+    placeholder: 'Full Name',
+  };
+  const touchedErrorProps = {
+    meta: {
+      touched: true,
+      error: 'An error message!'
+    }
+  };
+  const wrapper1 = shallow(<FieldGroup {...props} />);
+  const wrapper2 = shallow(<FieldGroup {...props} {...touchedErrorProps} />);
+  const wrapper3 = shallow(<FieldGroup />);
+
+  describe('rendering', () => {
+    describe('bootstrap components', () => {
+      it('should render <FormGroup />', () => {
+        expect(wrapper1.find('FormGroup').length).toBe(1);
+      });
+      it('should render <ControlLabel>', () => {
+        expect(wrapper1.find('ControlLabel').length).toBe(1);
+      });
+      it('should render <FormControl>', () => {
+        expect(wrapper1.find('FormControl').length).toBe(1);
+      });
+      it('should render <HelpBlock>', () => {
+        expect(wrapper1.find('HelpBlock').length).toBe(1);
+      });
+      it('should render helpText in <HelpBlock>', () => {
+        expect(wrapper1.find('HelpBlock').html()).toContain(props.helpText);
+      });
+      it('should not render <Popover> if not touched or errors present', () => {
+        expect(wrapper1.find('Popover').length).toBe(0);
+      });
+      it('should render <Popover> if touched and errors present', () => {
+        expect(wrapper2.find('Popover').length).toBe(1);
+      });
+      it('should render errorText in <Popover>', () => {
+        expect(wrapper2.find('Popover').html()).toContain(touchedErrorProps.meta.error);
+      });
+    });
+    describe('type = "text" vs. type = "textarea"', () => {
+      it('should render <input> if type = "text"', () => {
+        expect(wrapper1.html()).toContain('input type="text"');
+      });
+      it('should render a <textarea> if type = "textarea"', () => {
+        wrapper1.setProps({ type: 'textarea' });
+        expect(wrapper1.html()).toContain('</textarea>');
+      });
+    });
+  });
+  describe('props', () => {
+    it('should not load props if no props', () => {
+      expect(wrapper1.html()).not.toBe(wrapper3.html());
+    });
+  });
+});
