f242a50564d50ae5819d1ca0aba1bbae7a0f1760
Aiden Nguyen <anguyen@twilio.com>
2017-08-17 14:28:47 -0700

    Add component testing for campaign page

---
+++ test/client/components/campaign.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import CampaignPage from '../../../public/src/components/campaign/campaign';
+
+describe('<CampaignPage />', () => {
+  const props = {
+    handleSubmit: jest.fn(),
+    history: {
+      goBack: jest.fn()
+    },
+    destroy: jest.fn(),
+    saveNewCampaign: jest.fn()
+  };
+  const wrapper = shallow(<CampaignPage {...props} />);
+
+  describe('<CampaignPage /> rendering', () => {
+    it('1 form should be rendered', () => {
+      expect(wrapper.find('form').length).toBe(1);
+    });
+    it('should render 3 fields when scripts and contact_lists are populated ', () => {
+      expect(wrapper.find('Field').length).toBe(3);
+    });
+    it('should render 5 fields when scripts and contact_lists are populated ', () => {
+      wrapper.setProps({ scripts: {}, contact_lists: {} });
+      expect(wrapper.find('Field').length).toBe(5);
+    });
+    it('should render 1 button tool bar', () => {
+      expect(wrapper.find('ButtonToolbar').length).toBe(1);
+    });
+    it('should render 4 buttons', () => {
+      expect(wrapper.find('Button').length).toBe(4);
+    });
+  });
+});
