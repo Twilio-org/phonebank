fc830e7b6e6cf2415d00e93a7d49594b3eaf941c
Allie Glotfelty <aglotfelty@twilio.com>
2017-08-17 15:57:25 -0700

    Rename script_question_list component

---
+++ public/src/components/script/script_question_list.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const QuestionList = (props) => {
+  const { question } = props;
+  console.log('question is: ', question);
+
+  return (
+    <li>{question.title}
+      <ul>
+        <li>Description: {question.description}</li>
+        <li>Type: {question.type}</li>
+        <li>Responses: {question.responses ? question.responses : 'Text Paragraph'}</li>
+      </ul>
+    </li>
+  );
+};
+
+export default QuestionList;
+
