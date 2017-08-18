89676bd85cd739f8130703c70b1ce126cb7e8d68
Aiden Nguyen <anguyen@twilio.com>
2017-08-18 11:12:00 -0700

    Add 2 GET requests test to fetch all scripts and contact lists

--- test/client/actions/admin_add_campaign.test.js
+++ test/client/actions/admin_add_campaign.test.js
@@ -14,7 +14,8 @@ const { listFixture: contactListFixtures,
         mapFixture: contactListFixture } = fixtures.contactListFixtures;
 
 describe('Add Campaign actions', () => {
-  let mock, store;
+  let mock;
+  let store;
   beforeEach(() => {
     store = mockStore(initialState);
   });
@@ -62,6 +63,54 @@ describe('Add Campaign actions', () => {
         const thirdObjectProps = checkObjectProps(expectedContactListProps, payload[2]);
         expect(payload.length).toBe(3);
         expect(thirdObjectProps && secondObjectProps && firstObjectProps).toBe(true);
+      });
+    });
+  });
+
+  describe('fetchAllScripts: ', () => {
+    mock = new MockAdapter(axios);
+    beforeEach(() => {
+      mock.onGet('/scripts').reply(200, scriptListFixtures);
+    });
+
+    afterEach(() => {
+      mock.reset();
+    });
+    describe('axios GET requests: ', () => {
+      it('should add the appropriate action to the store: ', () => {
+        const expectedAction = setScriptOptions(scriptListFixtures);
+        return store.dispatch(fetchAllScripts())
+          .then(() => {
+            const dispatchedActions = store.getActions();
+            const { type, payload } = dispatchedActions[0];
+            expect(dispatchedActions[0]).toEqual(expectedAction);
+            expect(type).toBe('SET_CAMPAIGN_FORM_SCRIPT');
+            expect(payload).toEqual(scriptListFixtures);
+          });
+      });
+    });
+  });
+
+  describe('fetchAllContactLists: ', () => {
+    mock = new MockAdapter(axios);
+    beforeEach(() => {
+      mock.onGet('/contactLists').reply(200, contactListFixtures);
+    });
+
+    afterEach(() => {
+      mock.reset();
+    });
+    describe('axios GET requests: ', () => {
+      it('should add the appropriate action to the store: ', () => {
+        const expectedAction = setContactListOptions(contactListFixtures);
+        return store.dispatch(fetchAllContactLists())
+          .then(() => {
+            const dispatchedActions = store.getActions();
+            const { type, payload } = dispatchedActions[0];
+            expect(dispatchedActions[0]).toEqual(expectedAction);
+            expect(type).toBe('SET_CAMPAIGN_FORM_CONTACT_LIST');
+            expect(payload).toEqual(contactListFixtures);
+          });
       });
     });
   });
