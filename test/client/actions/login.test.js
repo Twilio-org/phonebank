6aaf15674cdfacd51c207efcd01828ef73f65539
Allie Glotfelty <aglotfelty@twilio.com>
2017-08-17 19:11:49 -0700

    Update tests in login.test.js

--- test/client/actions/login.test.js
+++ test/client/actions/login.test.js
@@ -19,7 +19,10 @@ const authStatus = {
   id: null
 };
 
-const store = mockStore(initialState, authStatus);
+const loginUserInfo = {
+  email: 'harry@hogwarts.com',
+  password: '1234'
+};
 
 class LocalStorageMock {
   constructor() {
@@ -32,67 +35,90 @@ class LocalStorageMock {
     return this._store[key] || null;
   }
   setItem(key, value) {
-    this._store[key] = value.toString();
+    this._store[key] = value;
   }
   removeItem(key) {
     delete this._store[key];
   }
 }
 
+let mocker;
+let store;
 const history = {
-  push: function (value) {
-    console.log(value);
-  }
+  push: jest.fn()
 };
 
 global.localStorage = new LocalStorageMock();
 
 const user = {
+  id: 1,
   first_name: 'Oscar',
   last_name: 'Grouch',
   email: 'oscar@g.com',
   phone_number: '15555555555'
 };
 
+const loginUserResponse = {
+  token: 'token',
+  id: 1
+};
+
 describe('loginActions', () => {
   describe('setUserAuthCredentials', () => {
+    const { type, payload } = setUserAuthCredentials(user);
     it('should have a type of "SET_AUTH_JWT_FULFILLED"', () => {
-      expect(setUserAuthCredentials().type).toEqual('SET_AUTH_JWT_FULFILLED');
+      expect(type).toEqual('SET_AUTH_JWT_FULFILLED');
     });
     it('should pass on the correct payload we pass in', () => {
-      expect(setUserAuthCredentials(user).payload).toEqual(user);
+      expect(payload).toEqual(user);
     });
   });
   describe('clearAuthCredentials', () => {
+    const { type } = clearAuthCredentials();
     it('should have a type of "CLEAR_AUTH"', () => {
-      expect(clearAuthCredentials().type).toEqual('CLEAR_AUTH');
+      expect(type).toEqual('CLEAR_AUTH');
     });
   });
   describe('logout', () => {
+    const { type } = logout();
     it('should have a type of "LOGOUT_USER"', () => {
-      expect(logout().type).toEqual('LOGOUT_USER');
+      expect(type).toEqual('LOGOUT_USER');
     });
   });
   describe('loginUser', () => {
+    mocker = new MockAdapter(axios);
     beforeEach(() => {
-      const mock = new MockAdapter(axios);
-      const newUser = {
-        email: 'oscar@g.com',
-        password: '1234'
-      };
-      mock.onPost('/auth/login', newUser, history).reply(200, {
-        token: 'token',
-        id: 1
-      });
-      setTimeout(loginUser(newUser, history), 5000);
-      // setTimeout(() => done(), 10000);
+      store = mockStore(initialState, authStatus);
+      mocker.onPost('/auth/login').reply(200, loginUserResponse);
     });
-    test('updating localStorage with loginUser', () => {
-      console.log('global.localstorage is: ', global.localStorage);
-      expect(global.localStorage.auth_token).toEqual('token');
+    afterEach(() => {
+      mocker.reset();
     });
-    test('history should have "/" as argument', () => {
-      expect(history[0]).toEqual('/');
+    it('should add the "auth_token" to localStorage', () => {
+      return store.dispatch(loginUser(loginUserInfo, history))
+        .then(() => {
+          const { token } = loginUserResponse;
+          console.log('the token is: ', token);
+          expect(global.localStorage.setItem('auth_token', 'token')).toBeCalled();
+          // expect(global.localStorage.getItem[0]).toEqual(token);
+        });
+    });
+    it('should dispatch the setUserAuthCredentials action to the store', () => {
+      return store.dispatch(loginUser(loginUserInfo, history))
+        .then(() => {
+          const { type } = setUserAuthCredentials(user);
+          const { id } = loginUserResponse;
+          const actions = store.getActions();
+          expect(actions[0].type).toEqual(type);
+          expect(actions[0].payload.id).toEqual(id);
+        });
+    });
+    it('should call history.push()', () => {
+      return store.dispatch(loginUser(loginUserInfo, history))
+        .then(() => {
+          expect(history.push).toBeCalled();
+          expect(history.push.mock.calls[0]).toEqual(['/']);
+        });
     });
   });
 });
