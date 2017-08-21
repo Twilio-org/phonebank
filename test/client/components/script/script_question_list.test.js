d10e1d8d9dfb69e4cd8b125f87384913fc077aa6
Allie Glotfelty <aglotfelty@twilio.com>
2017-08-21 10:11:17 -0600

    Update script_question_list with client helpers

--- test/client/components/script/script_question_list.test.js
+++ test/client/components/script/script_question_list.test.js
@@ -1,49 +1,10 @@
-function checkObjectProps(expectedProps, obj) {
-  return expectedProps.reduce((accum, curr) => {
-    const propertyExists = Object.prototype.hasOwnProperty.call(obj, curr);
-    return accum && propertyExists;
-  }, true);
-}
+import fixtures from '../../client_fixtures';
+import { checkObjectProps } from '../../client_test_helpers.js';
+
+const { scriptQuestionsFixture: questions } = fixtures.scriptViewFixtures;
 
 const props = {
-  questions: [
-    {
-      id: 4,
-      title: 'Age',
-      description: 'What is your age range?',
-      type: 'singleselect',
-      responses: '0-10,11-20,21-40,41-60,61+',
-      created_at: '2017-08-11T21:36:45.387Z',
-      updated_at: '2017-08-11T21:36:45.387Z',
-      script_id: 1,
-      question_id: 3,
-      sequence_number: 1
-    },
-    {
-      id: 5,
-      title: 'Hobbies',
-      description: 'What are your hobbies?',
-      type: 'multiselect',
-      responses: 'swimming,running,biking,sleeping,eating,weaving',
-      created_at: '2017-08-11T21:36:45.398Z',
-      updated_at: '2017-08-11T21:36:45.398Z',
-      script_id: 1,
-      question_id: 2,
-      sequence_number: 2
-    },
-    {
-      id: 6,
-      title: 'Ice Cream',
-      description: 'How do you feel about ice cream?',
-      type: 'paragraph',
-      responses: null,
-      created_at: '2017-08-11T21:36:45.514Z',
-      updated_at: '2017-08-11T21:36:45.514Z',
-      script_id: 1,
-      question_id: 1,
-      sequence_number: 3
-    }
-  ]
+  questions
 };
 
 describe('QuestionList component', () => {
