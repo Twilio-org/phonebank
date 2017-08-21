b3eb74f7978e5535fb1305ad60616b9216ca1b10
Allie Glotfelty <aglotfelty@twilio.com>
2017-08-21 13:54:54 -0600

    Refactor account_info action test with client_test_helpers

--- test/client/actions/account_info.test.js
+++ test/client/actions/account_info.test.js
@@ -1,5 +1,3 @@
-import configureMockStore from 'redux-mock-store';
-import thunk from 'redux-thunk';
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 import { setAccountInfo, fetchUser } from '../../../public/src/actions/account_info';
