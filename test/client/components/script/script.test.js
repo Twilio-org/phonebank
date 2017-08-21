2d660f4ce0361eb9c149a4cf0fb98f7fa39fd9f0
Allie Glotfelty <aglotfelty@twilio.com>
2017-08-21 09:59:56 -0600

    Added fixtures for script view tests

--- test/client/components/script/script.test.js
+++ test/client/components/script/script.test.js
@@ -1,66 +1,21 @@
 import { fetchScript, fetchScriptQuestions } from '../../../../public/src/actions/script';
+import fixtures from '../../client_fixtures';
+import { checkObjectProps } from '../../client_test_helpers.js';
+
+const { scriptQuestionsFixture: questions,
+        admin_scripts: scripts } = fixtures.scriptViewFixtures;
 
 const props = {
   id: 1,
-  admin_scripts: {
-    current_script: {
-      name: 'ScriptName',
-      body: 'ScriptBody',
-      description: 'ScriptDescription'
-    }
-  },
-  questions: [
-    {
-      id: 4,
-      title: 'Age',
-      description: 'What is your age range?',
-      type: 'singleselect',
-      responses: '0-10,11-20,21-40,41-60,61+',
-      created_at: '2017-08-11T21:36:45.387Z',
-      updated_at: '2017-08-11T21:36:45.387Z',
-      script_id: 1,
-      question_id: 3,
-      sequence_number: 1
-    },
-    {
-      id: 5,
-      title: 'Hobbies',
-      description: 'What are your hobbies?',
-      type: 'multiselect',
-      responses: 'swimming,running,biking,sleeping,eating,weaving',
-      created_at: '2017-08-11T21:36:45.398Z',
-      updated_at: '2017-08-11T21:36:45.398Z',
-      script_id: 1,
-      question_id: 2,
-      sequence_number: 2
-    },
-    {
-      id: 6,
-      title: 'Ice Cream',
-      description: 'How do you feel about ice cream?',
-      type: 'paragraph',
-      responses: null,
-      created_at: '2017-08-11T21:36:45.514Z',
-      updated_at: '2017-08-11T21:36:45.514Z',
-      script_id: 1,
-      question_id: 1,
-      sequence_number: 3
-    }
-  ],
+  scripts,
+  questions,
   fetchScript,
   fetchScriptQuestions
 };
 
-function checkObjectProps(expectedProps, obj) {
-  return expectedProps.reduce((accum, curr) => {
-    const propertyExists = Object.prototype.hasOwnProperty.call(obj, curr);
-    return accum && propertyExists;
-  }, true);
-}
-
 describe('<ScriptPage />', () => {
-  const { id, questions, admin_scripts } = props;
-  const { current_script } = admin_scripts;
+  const { id, questions, scripts } = props;
+  const { current_script } = scripts;
   describe('ScriptPage props and methods', () => {
     describe('props', () => {
       describe('script id prop', () => {
