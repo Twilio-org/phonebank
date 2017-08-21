1e24c6eab10d9ef696fc50d439ebcb2d37b1b9ab
Allie Glotfelty <aglotfelty@twilio.com>
2017-08-21 10:00:05 -0600

    Added fixtures for script view tests

--- test/client/reducers/script.test.js
+++ test/client/reducers/script.test.js
@@ -1,57 +1,8 @@
-import { scriptInfoReducer, scriptQuestionsReducer, SET_SCRIPT_INFO, SET_SCRIPT_QUESTIONS } from '../../../public/src/reducers/script';
+import { scriptQuestionsReducer, SET_SCRIPT_QUESTIONS } from '../../../public/src/reducers/script';
+import fixtures from '../client_fixtures';
 
-const initialScriptState = {
-  name: null,
-  body: null,
-  description: null
-};
-
+const { scriptQuestionsFixture } = fixtures.scriptViewFixtures;
 const initialScriptQuestionState = [];
-
-const newScript = {
-  name: 'ScriptName',
-  body: 'ScriptBody',
-  description: 'ScriptDescription'
-};
-
-const scriptQuestions = [
-  {
-    id: 4,
-    title: 'Age',
-    description: 'What is your age range?',
-    type: 'singleselect',
-    responses: '0-10,11-20,21-40,41-60,61+',
-    created_at: '2017-08-11T21:36:45.387Z',
-    updated_at: '2017-08-11T21:36:45.387Z',
-    script_id: 1,
-    question_id: 3,
-    sequence_number: 1
-  },
-  {
-    id: 5,
-    title: 'Hobbies',
-    description: 'What are your hobbies?',
-    type: 'multiselect',
-    responses: 'swimming,running,biking,sleeping,eating,weaving',
-    created_at: '2017-08-11T21:36:45.398Z',
-    updated_at: '2017-08-11T21:36:45.398Z',
-    script_id: 1,
-    question_id: 2,
-    sequence_number: 2
-  },
-  {
-    id: 6,
-    title: 'Ice Cream',
-    description: 'How do you feel about ice cream?',
-    type: 'paragraph',
-    responses: null,
-    created_at: '2017-08-11T21:36:45.514Z',
-    updated_at: '2017-08-11T21:36:45.514Z',
-    script_id: 1,
-    question_id: 1,
-    sequence_number: 3
-  }
-];
 
 describe('scriptQuestionsReducer', () => {
   it('should return the initial state', () => {
@@ -60,7 +11,7 @@ describe('scriptQuestionsReducer', () => {
   it('should react to an action with the type SET_SCRIPT_QUESTIONS', () => {
     expect(scriptQuestionsReducer(initialScriptQuestionState, {
       type: SET_SCRIPT_QUESTIONS,
-      payload: scriptQuestions
-    }).questions).toEqual(scriptQuestions);
+      payload: scriptQuestionsFixture
+    }).questions).toEqual(scriptQuestionsFixture);
   });
 });
