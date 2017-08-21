5c5d4d8d519b0f9c1246180102a95af7d9600993
Allie (Holmes) Glotfelty <allie.glotfelty@gmail.com>
2017-08-21 15:21:36 -0600

    Script View (#79)
    Refactor of script files
    Script View Component
    Script Question List Component
    Script View Container
    Script View Actions
    Script View Reducers
    Script View tests of all above

--- test/client/client_fixtures.js
+++ test/client/client_fixtures.js
@@ -206,6 +206,59 @@ export default {
       created_at: '2017-08-15T21:35:30.321Z',
       updated_at: '2017-08-15T21:35:30.321Z'
     }
+  },
+  scriptViewFixtures: {
+    scriptFixture: {
+      id: 1,
+      name: 'ScriptName',
+      body: 'ScriptBody',
+      description: 'ScriptDescription',
+      created_at: '2017-08-11T21:36:45.366Z',
+      updated_at: '2017-08-11T21:36:45.366Z'
+    },
+    current_script: {
+      name: 'ScriptName',
+      body: 'ScriptBody',
+      description: 'ScriptDescription'
+    },
+    scriptQuestionsFixture: [
+      {
+        id: 4,
+        title: 'Age',
+        description: 'What is your age range?',
+        type: 'singleselect',
+        responses: '0-10,11-20,21-40,41-60,61+',
+        created_at: '2017-08-11T21:36:45.387Z',
+        updated_at: '2017-08-11T21:36:45.387Z',
+        script_id: 1,
+        question_id: 3,
+        sequence_number: 1
+      },
+      {
+        id: 5,
+        title: 'Hobbies',
+        description: 'What are your hobbies?',
+        type: 'multiselect',
+        responses: 'swimming,running,biking,sleeping,eating,weaving',
+        created_at: '2017-08-11T21:36:45.398Z',
+        updated_at: '2017-08-11T21:36:45.398Z',
+        script_id: 1,
+        question_id: 2,
+        sequence_number: 2
+      },
+      {
+        id: 6,
+        title: 'Ice Cream',
+        description: 'How do you feel about ice cream?',
+        type: 'paragraph',
+        responses: null,
+        created_at: '2017-08-11T21:36:45.514Z',
+        updated_at: '2017-08-11T21:36:45.514Z',
+        script_id: 1,
+        question_id: 1,
+        sequence_number: 3
+      }
+    ]
   }
 };
 
---
+++ test/client/components/script/script_question_list.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import fixtures from '../../client_fixtures';
+import { checkObjectProps } from '../../client_test_helpers.js';
+import QuestionList from '../../../../public/src/components/script/script_question_list';
+
+const { scriptQuestionsFixture } = fixtures.scriptViewFixtures;
+
+const props = {
+  question: scriptQuestionsFixture[0]
+};
+
+describe('QuestionList component', () => {
+  const wrapper = shallow(<QuestionList {...props} />);
+  describe('QuestionList rendering', () => {
+    it('should render one question in the list', () => {
+      const ulElements = wrapper.find('ul').length;
+      expect(ulElements).toBe(1);
+    });
+    it('should render a list of the question description, title and responses within the question list element', () => {
+      const liElements = wrapper.find('li').length;
+      expect(liElements).toBe(4);
+    });
+  });
+  describe('Expected props for <QuestionList />', () => {
+    const wrapper = shallow(<QuestionList {...props} />).instance();
+    const expectedProps = Object.keys(props);
+    it('should have the expected props', () => {
+      const actualProps = wrapper.props;
+      expect(checkObjectProps(expectedProps, actualProps)).toBe(true);
+    });
+  });
+});
