1366e19f96d88effaa8f95cb91d6ede2c78a5bf7
Andrea Oneto <aoneto@twilio.com>
2017-08-22 13:19:08 -0700

    refactored helper to only check for missing headers now returns an error obj that can be handled with a try/catch block in the controller
    expects a buffer from req.forms.data

--- server/util/csv_valid_parse.js
+++ server/util/csv_valid_parse.js
@@ -2,10 +2,9 @@ import parse from 'csv-parse/lib/sync';
 
 function validateRequiredHeaders(actualHeadersArray) {
   const errors = {
-    mult: [],
     missing: []
   };
-  const { mult, missing } = errors;
+  const { missing } = errors;
   const reqHeaders = {
     external_id: 0,
     first_name: 0,
@@ -23,38 +22,21 @@ function validateRequiredHeaders(actualHeadersArray) {
     if (!reqHeaders[header]) {
       missing.push(header);
     }
-    if (reqHeaders[header] > 1) {
-      const count = reqHeaders[header];
-      const problem = {
-        header,
-        count
-      };
-      mult.push(problem);
-    }
   });
-  if (!mult.length && !missing.length) {
+  if (!missing.length) {
     return true;
   }
   return errors;
 }
 
 function customError(name, message) {
-  const error = { error: name, message };
-  throw error;
+  return new Error(message);
 }
 
-function handleAllErrors(errorList) {
-  let errors;
-  if (errorList.length === 1) {
-    const [only] = errorList;
-    const { name, message } = only;
-    return customError(name, message);
-  }
-  errorList.forEach((error) => {
-    const { name, message } = error;
-    errors.push(customError(name, message));
-  });
-  return errors;
+function handleError(errorList) {
+  const [only] = errorList;
+  const { name, message } = only;
+  return customError(name, message);
 }
 
 function handleValidationError(errorsObj) {
@@ -62,18 +44,12 @@ function handleValidationError(errorsObj) {
   const messages = [];
   props.forEach((prop) => {
     if (errorsObj[prop].length) {
-      if (prop === 'mult') {
-        errorsObj.prop.forEach((errorInstance) => {
-          const { count, header } = errorInstance;
-          messages.push({ name: 'replicated header name(s)', message: `replications: there are ${count} ${header} headers.` });
-        });
-      }
       if (prop === 'missing') {
-        messages.push({ name: 'missing required headers', message: `missing: ${errorsObj.missing.join(', ')}` });
+        messages.push({ name: 'missing required headers', message: `missing required header(s): ${errorsObj.missing.join(', ')}` });
       }
     }
   });
-  return handleAllErrors(messages);
+  return handleError(messages);
 }
 
 export default function parseCSVThenValidateHeaders(uploadedCsv) {
@@ -82,10 +58,12 @@ export default function parseCSVThenValidateHeaders(uploadedCsv) {
   const [headerCheck] = csvObjects;
   const headers = Object.keys(headerCheck);
   const result = validateRequiredHeaders(headers);
-  return result ? csvObjects : handleValidationError(result);
+  return result && typeof result !== 'object' ? csvObjects : handleValidationError(result);
 }
 
 // NOTE: may want to change to an async call (cb for db service calls)... or not
 // in controller: import the helper and invoke on 'uploadedCsv'
 // if results in error (e.g. not an array of objs), then return error
+
+// try and catch in controller to catch the error (if any)
 
