708e28aaea75515e2b8fbb6c91f8220ccb6bd985
Allie Glotfelty <aglotfelty@twilio.com>
2017-08-24 17:22:29 -0700

    Remove do_not_call and is_invalid_number from updateContactById service

--- server/db/services/contacts.js
+++ server/db/services/contacts.js
@@ -45,14 +45,11 @@ export default {
       last_name: params.last_name,
       phone_number: params.phone_number,
       email: params.email,
-      external_id: params.external_id,
-      do_not_call: params.do_not_call,
-      is_invalid_number: params.is_invalid_number
+      external_id: params.external_id
     };
-    return new Contact()
-      .where({ id })
+    return new Contact({ id })
       .save(extractedParams, {
-        method: 'update'
+        patch: true
       });
   },
 
