285a240a2bb90e07c8960973ca071d01f66195b6
Allie Glotfelty <aglotfelty@twilio.com>
2017-08-28 14:32:39 -0700

    Add campaign association to users model

--- server/db/models/users.js
+++ server/db/models/users.js
@@ -1,7 +1,11 @@
 import db from '../db';
+import Campaign from './campaigns';
 
 export default db.Model.extend({
   tableName: 'users',
   bcrypt: { field: 'password_hash' },
-  hasTimestamps: true
+  hasTimestamps: true,
+  campaigns() {
+    return this.belongsToMany(Campaign);
+  }
 });
