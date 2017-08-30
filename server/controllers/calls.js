bd02641c16f8ebb66a6dec90f97eec78a759ce17
Andrea Oneto <aoneto@twilio.com>
2017-08-30 14:29:16 -0700

    created methods for callsService (recordAttempt, createNewAttempt, getCallById) created controller for recording an attempted call TODO: will hook up to preceeding tickets after PRs are merged

--- server/controllers/calls.js
+++ server/controllers/calls.js
@@ -2,17 +2,16 @@ import callsService from '../db/services/calls';
 import usersService from '../db/services/users';
 
 function userHasJoinedCampaign(userId, campaignId) {
-  return usersService.getUserCampaigns({ id: user_id })
+  return usersService.getUserCampaigns({ id: userId })
     .then((campaigns) => {
-      const userCampaigns = campaigns.models.filter(campaign => campaign.id === campaignId)
+      const userCampaigns = campaigns.models.filter(campaign => campaign.id === campaignId);
 
-    return !!userCampaigns.length
-  });
+      return !!userCampaigns.length;
+    });
 }
 
 function outcomeIsValid(outcome) {
   const validOutcomes = [
-    'PENDING',
     'ANSWERED',
     'BAD_NUMBER',
     'DO_NOT_CALL',
@@ -43,6 +42,15 @@ export function assignCall(req, res) {
   return res.status(401).json({ message: 'User has not joined that campaign' });
 }
 
+function updateNoCallContact(outcome, id) {
+  const { updateContactDoNotCallById, updateContactInvalidNumberById } = usersService;
+  const outcomeMap = {
+    DO_NOT_CALL: updateContactDoNotCallById,
+    BAD_NUMBER: updateContactInvalidNumberById
+  };
+  return outcomeMap[outcome]({ id });
+}
+
 export function recordAttempt(req, res) {
   const { outcome, notes } = req.body;
   const { call_id: id } = req.params;
@@ -55,12 +63,34 @@ export function recordAttempt(req, res) {
 
   if (userHasJoinedCampaign(user_id, user_campaign_id)) {
     callsService.getCallById({ id })
-      .then(() => {
-        callsService.recordAttempt({ id, outcome, notes })
+      .then((call) => {
+        const { contact_id } = call;
+        return callsService.recordAttempt({ id, outcome, notes })
         .then((attemptedCall) => {
+          let { attempt_num } = attemptedCall;
+          attempt_num = parseInt(attempt_num, 10);
           if (outcome === 'DO_NOT_CALL' || outcome === 'BAD_NUMBER') {
-            
+            return updateNoCallContact(outcome, contact_id)
+              .then(() => {
+                res.status(200).json({ message: 'contact and call log updated successfully' });
+              })
+              .catch(err => console.log(err));
           }
+          if (outcome === 'LEFT_MSG' || outcome === 'NO_ANSWER' || outcome === 'INCOMPLETE') {
+            if (attempt_num < 3) {
+              attempt_num = (attempt_num + 1).toString();
+              const updatedCall = {
+                ...attemptedCall,
+                attempt_num
+              };
+              callsService.createNewAttempt(updatedCall)
+                .then()
+                .catch(err => console.log(err));
+            }
+            return res.status(200).json({ message: 'call log updated successfully' });
+          }
+          // ANSWERED:
+          return res.status(200).json({ message: 'call log updated successfully' });
         })
         .catch(err => console.log(err));
       })
