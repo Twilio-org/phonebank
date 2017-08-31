e70acb0a19b86e9b153361cba25d49ea165e473e
Allie Glotfelty <aglotfelty@twilio.com>
2017-08-31 15:59:42 -0700

    Add responses model and update calls and questions models

--- server/db/models/questions.js
+++ server/db/models/questions.js
@@ -1,10 +1,14 @@
 import db from '../db';
 import Script from './scripts';
+import Response from './responses';
 
 export default db.Model.extend({
   tableName: 'questions',
   hasTimestamps: true,
   scripts() {
     return this.belongsToMany(Script);
+  },
+  responses() {
+    return this.hasMany(Response);
   }
 });
