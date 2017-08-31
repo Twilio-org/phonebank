d3e5d7707c49123dcfb5eae2f95f3e2dbcdd2147
Jadzia <jadzia@twilio.com>
2017-08-31 14:20:18 -0700

    Add catch handlers to promise returns in call service.

--- server/db/services/calls.js
+++ server/db/services/calls.js
@@ -12,13 +12,16 @@ export default {
 
     return new Call()
       .where({ campaign_id, status: 'AVAILABLE' })
-      .fetch().then((call) => {
+      .fetch()
+      .then((call) => {
         if (call) {
           return call.save({ user_id, status: 'ASSIGNED' }, { patch: true })
-            .then(savedCall => savedCall);
+            .then(savedCall => savedCall)
+            .catch(err => console.log('Error in call service when assigning to user: ', err));
         }
 
         return null;
-      });
+      })
+      .catch(err => console.log('Error in call service when finding call to assign:', err));
   }
 };
