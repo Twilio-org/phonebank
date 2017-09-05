f439a14f9bcadb5fc0c5d0f05addd70175652320
Jadzia <jadzia@twilio.com>
2017-09-05 16:50:43 -0700

    Add tests

--- test/client/components/campaign/joined_campaigns.test.js
+++ test/client/components/campaign/joined_campaigns.test.js
@@ -18,8 +18,8 @@ const props = {
   setCurrentCampaign: jest.fn()
 };
 describe('<JoinedCampaigns />', () => {
-  const wrapper = shallow(<JoinedCampaigns {...props} />);
   describe('rendering', () => {
+    const wrapper = shallow(<JoinedCampaigns {...props} />);
     it('should render 1 <Banner>', () => {
       expect(wrapper.find('Banner').length).toBe(1);
     });
@@ -29,14 +29,36 @@ describe('<JoinedCampaigns />', () => {
     it('should render 1 <SidebarList>', () => {
       expect(wrapper.find('SidebarList').length).toBe(1);
     });
-    it('should render 1 <CurrentCampaign>', () => {
-      expect(wrapper.find('CurrentCampaign').length).toBe(1);
-    });
   });
   describe('Mounting', () => {
     it('Should call the fetchQuestion action on mount', () => {
       mount(<JoinedCampaigns {...props} />);
       expect(props.fetchCampaignsByUser).toHaveBeenCalled();
+    });
+  });
+  describe('<CurrentCampaign> render switch', () => {
+    it('should render 1 <CurrentCampaign> when elements are present in current_campaign prop', () => {
+      const campaignsPresentProps = {
+        history: {
+          goBack: jest.fn(),
+          location: { pathname: ''}
+        },
+        auth: { id: 1 },
+        fetchCampaignsByUser: jest.fn(),
+        current_campaign: {},
+        joined_campaigns: [1],
+        account_info: {
+          first_name: "Test",
+          last_name: "Tests"
+        },
+        setCurrentCampaign: jest.fn()
+      };
+      const wrapper = shallow(<JoinedCampaigns {...campaignsPresentProps} />);
+      expect(wrapper.find('CurrentCampaign').length).toBe(1);
+    });
+    it('should render 0 <CurrentCampaign> when no elements are present in current_campaign prop', () => {
+      const wrapper = shallow(<JoinedCampaigns {...props} />);
+      expect(wrapper.find('CurrentCampaign').length).toBe(0);
     });
   });
 });
