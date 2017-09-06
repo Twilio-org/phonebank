812cb9cd8c6f20ea1a11e7ddbaa3f98281f98ca8
Allie Glotfelty <aglotfelty@twilio.com>
2017-09-06 12:07:01 -0700

    Remove current_call fixture - no longer needed

--- test/client/client_fixtures.js
+++ test/client/client_fixtures.js
@@ -371,21 +371,6 @@ export default {
       created_at: '2017-08-15T21:35:30.321Z',
       updated_at: '2017-08-15T21:35:30.321Z'
     },
-    currentCall: {
-      id: 2,
-      campaign_id: 1,
-      contact_id: 1,
-      attempt_num: 1,
-      user_id: 1,
-      status: 'AVAILABLE',
-      outcome: 'PENDING',
-      notes: null,
-      call_sid: null,
-      call_started: null,
-      call_ended: null,
-      created_at: '2017-08-15T21:35:30.321Z',
-      updated_at: '2017-08-15T21:35:30.321Z'
-    },
     assignedCall: {
       id: 1,
       campaign_id: 1,
