8f7db7542622d42be623b5a69bfc93717de1d301
Andrea Oneto <aoneto@twilio.com>
2017-09-06 08:29:38 -0700

    finished all component testing for call sidebar components

--- public/src/components/calls/side_bar_form.jsx
+++ public/src/components/calls/side_bar_form.jsx
@@ -1,27 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Row, Col } from 'react-bootstrap';
 import { Field } from 'redux-form';
 
 import FieldGroup from '../../components/common/form/field_group';
 
-export default class SideBarForm extends Component {
-  render() {
-    const { handleSubmit } = this.props;
-    const inputFieldProps = {
-      label: 'notes',
-      name: 'Notes'
-    };
-    return (
-      <div>
-        <form onSubmit={handleSubmit}>
-          <hr />
-          <Row>
-            <Col sm={11}>
-              <Field component={FieldGroup} type="textarea" {...inputFieldProps} />
-            </Col>
-          </Row>
-        </form>
-      </div>
-    );
-  }
-}
+const SideBarForm = (props) => {
+  const { handleSubmit } = props;
+  const inputFieldProps = {
+    label: 'notes',
+    name: 'Notes'
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <hr />
+        <Row>
+          <Col sm={11}>
+            <Field component={FieldGroup} type="textarea" {...inputFieldProps} />
+          </Col>
+        </Row>
+      </form>
+    </div>
+  );
+};
+
+SideBarForm.displayName = 'SideBarForm';
+
+export default SideBarForm;
