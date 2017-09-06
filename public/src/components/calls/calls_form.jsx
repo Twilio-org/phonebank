324ad6075f3085457acbf6551fce89593337a60c
Lupita Davila <lupita.dav@gmail.com>
2017-09-06 13:22:50 -0700

    Complete code change request

--- public/src/components/calls/calls_form.jsx
+++ public/src/components/calls/calls_form.jsx
@@ -4,11 +4,12 @@ import CallQuestion from './call_question';
 export default class CallsForm extends Component {
   render() {
     const { questions, change, form, status } = this.props;
+    // ASSIGNED is only status we recieve that disables form
     return (
       <form>
         <h3>Questions</h3>
         <hr />
-        <fieldset disabled={status !== 'IN_PROGRESS' || status !== 'HUNG_UP'}>
+        <fieldset disabled={status === 'ASSIGNED'}>
           {
             questions && questions.map((question, i) => (
               <CallQuestion
