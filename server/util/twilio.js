2704925f04aa5cce073a8378da5aa57de836aa07
Allie Glotfelty <aglotfelty@twilio.com>
2017-09-07 09:35:04 -0700

    Added console logs to twilio module to pass linting

--- server/util/twilio.js
+++ server/util/twilio.js
@@ -1,6 +1,9 @@
-import twilio from 'twilio';
+import Twilio from 'twilio';
 
 // Account SID and Auth Token are stored as environmental variables
 // Twilio Node module will check for them automatically upon initialization
-const twilioClient = new twilio();
+const twilioClient = new Twilio();
 const CALLER_ID = process.env.TWILIO_CALLER_ID;
+
+console.log(twilioClient);
+console.log(CALLER_ID);
