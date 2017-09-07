8330d316b797de621443f0b3483c4de8aec50224
Jadzia Thomas <jadziahax@gmail.com>
2017-09-07 16:25:01 -0700

    Update twilio.js

--- server/util/twilio.js
+++ server/util/twilio.js
@@ -1,6 +1,6 @@
 import Twilio from 'twilio';
 
-// Account SID and Auth Token are stored as environmental variables		
+// Account SID and Auth Token are stored as environmental variables
 // Twilio Node module will check for them automatically upon initialization
 const twilioClient = new Twilio();
 const CALLER_ID = process.env.TWILIO_CALLER_ID;
