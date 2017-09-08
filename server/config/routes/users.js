49f63f7a4f43ac5ce7440e6bb0c4d90660090c70
Jadzia Thomas <jadziahax@gmail.com>
2017-09-08 15:12:40 -0700

    Ticket 119: Add utility method, controller, route config, and test. (#144)
    * Add utility method, controller, route config, and test

--- server/config/routes/users.js
+++ server/config/routes/users.js
@@ -3,6 +3,7 @@ import { addCampaignToUser,
          clearUserCallSIDField,
          deactivateUserById,
          getAllUsers,
+         getCallCompleteTwiml,
          getCallStartTwiml,
          getUserById,
          getUserCampaignAssociation,
@@ -23,7 +24,7 @@ router.route('/').get(getAllUsers);
 router.route('/:id').get(getUserById);
 router.route('/:id').put(updateUserById);
 router.route('/:id').patch(deactivateUserById);
-router.route('/:id/manage').put(manageUserById);
+router.route('/:id/callback').post(volunteerCallback);
 router.route('/:id/campaigns').post(addCampaignToUser);
 router.route('/:id/campaigns').get(getUserCampaigns);
 router.route('/:id/campaigns/:campaign_id').get(getUserCampaignAssociation);
@@ -32,7 +33,8 @@ router.route('/:id/campaigns/:campaign_id/calls').post(updateUserCallSIDField);
 router.route('/:id/campaigns/:campaign_id/calls').delete(clearUserCallSIDField);
 router.route('/:id/campaigns/:campaign_id/calls/:call_id').delete(releaseCall);
 router.route('/:id/campaigns/:campaign_id/calls/:call_id').put(recordAttempt);
+router.route('/:id/campaigns/:campaign_id/calls/bridge').post(getCallCompleteTwiml);
 router.route('/:id/campaigns/:campaign_id/calls/start').post(getCallStartTwiml);
-router.route('/:id/callback').post(volunteerCallback);
+router.route('/:id/manage').put(manageUserById);
 
 export default router;
