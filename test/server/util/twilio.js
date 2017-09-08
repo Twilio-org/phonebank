1362839bdfa7fc28953f90303d5503f42b6f97b5
Jadzia <jadzia@twilio.com>
2017-09-08 10:30:39 -0700

    Update test string for sayHelloUser TwiML generation function.

--- test/server/util/twilio.js
+++ test/server/util/twilio.js
@@ -7,7 +7,7 @@ describe('XML Generation', () => {
 
   it('should return TwiML that uses the user\'s first name and the campaign\'s name in the proper template', () => {
     expect(sayHelloUser(userFirstName, campaignName)).to.equal(
-      `<?xml version="1.0" encoding="UTF-8"?><Response><Say>Welcome, ${userFirstName}. You will be calling on behalf of ${campaignName}. Please follow the instructions on your computer screen to start calling.</Say><Play loop="0">https://api.twilio.com/cowbell.mp3</Play></Response>`
+      `<?xml version="1.0" encoding="UTF-8"?><Response><Say>Welcome, ${userFirstName}. You will be calling on behalf of ${campaignName}. Please follow the instructions on your computer screen to start calling.</Say><Play loop="0">http://com.twilio.music.classical.s3.amazonaws.com/ith_chopin-15-2.mp3</Play></Response>`
     );
   });
 });
