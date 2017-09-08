7e75b1ce71202ee678bde8e34b76b31133fc0bf0
Jadzia <jadzia@twilio.com>
2017-09-08 09:47:22 -0700

    change hold music.

--- server/util/twilio.js
+++ server/util/twilio.js
@@ -9,7 +9,7 @@ export function sayHelloUser(userFirstName, campaignName) {
   const VoiceResponse = Twilio.twiml.VoiceResponse;
   const helloUser = new VoiceResponse();
   helloUser.say(`Welcome, ${userFirstName}. You will be calling on behalf of ${campaignName}. Please follow the instructions on your computer screen to start calling.`);
-  helloUser.play({ loop: 0 }, 'https://api.twilio.com/cowbell.mp3');
+  helloUser.play({ loop: 0 }, 'http://com.twilio.music.classical.s3.amazonaws.com/ith_chopin-15-2.mp3');
   return helloUser.toString();
 }
 
