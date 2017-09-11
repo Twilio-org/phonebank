b6fa4d1b8c9d50092e22f80233cf1612f10e776d
Allie Glotfelty <aglotfelty@twilio.com>
2017-09-11 09:17:24 -0700

    Update url to campaigns from campaign

--- server/util/twilio.js
+++ server/util/twilio.js
@@ -1,7 +1,7 @@
 import Twilio from 'twilio';
-import dotenv from 'dotenv';
+// import dotenv from 'dotenv';
 
-dotenv.load();
+// dotenv.load();
 
 // Account SID and Auth Token are stored as environmental variables
 // Twilio Node module will check for them automatically upon initialization
@@ -9,12 +9,12 @@ const twilioClient = new Twilio();
 console.log('Twilio in twilio module is: ', Twilio);
 
 const CALLER_ID = process.env.TWILIO_CALLER_ID;
-const DEV_PATH = 'http://cfef812c.ngrok.io';
+const DEV_PATH = 'http://7e22fba8.ngrok.io';
 
 export function callVolunteer(user_id, campaign_id, phone_number) {
   return new Promise((resolve, reject) => {
     twilioClient.calls.create({
-      url: `${DEV_PATH}/users/${user_id}/campaign/${campaign_id}/calls/start`,
+      url: `${DEV_PATH}/users/${user_id}/campaigns/${campaign_id}/calls/start`,
       to: phone_number,
       from: CALLER_ID,
       statusCallback: `${DEV_PATH}/users/${user_id}/callback`,
