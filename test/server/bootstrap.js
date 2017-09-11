4fb63129b3966ec082f5db440d425def6029fb1a
Allie Glotfelty <aglotfelty@twilio.com>
2017-09-11 15:02:04 -0700

    Remove mockery setup and teardown

--- test/server/bootstrap.js
+++ test/server/bootstrap.js
@@ -1,19 +1,7 @@
-import mockery from 'mockery';
 import { setup, teardown, truncateAllTables } from '../../server/db/db';
-import twilioMock from './util/mock_twilio';
 
-before((done) => {
-  teardown().then(setup);
-  mockery.registerMock('twilio', twilioMock);
-  console.log('twilioMock in bootstrap is: ', twilioMock);
-  mockery.enable({ useCleanCache: true });
-  done();
-});
-after((done) => {
-  mockery.disable();
-  teardown();
-  done();
-});
+before(() => teardown().then(setup));
+after(teardown);
 export default function cleanUp(done) {
   truncateAllTables()
     .then(() => {
