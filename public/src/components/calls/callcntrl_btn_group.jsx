9218162e42c988c87564242f81bf2784d963dc43
Lupita Davila <lupita.dav@gmail.com>
2017-09-12 12:06:17 -0700

    Add logic so you can't submit answered call if not hung up

--- public/src/components/calls/callcntrl_btn_group.jsx
+++ public/src/components/calls/callcntrl_btn_group.jsx
@@ -3,9 +3,8 @@ import { Button } from 'react-bootstrap';
 
 const CallControl = (props) => {
   const { handler, text, outcome, status, htmlID } = props;
-  console.log('Call control props', props);
   const style = text === 'Hang Up' ? 'danger' : 'success';
-  const disableStatus = text === 'Hang Up' ? (outcome === 'PENDING' || status !== 'IN_PROGRESS') : (outcome === 'PENDING' && status !== 'HUNG_UP');
+  const disableStatus = text === 'Hang Up' ? (outcome === 'PENDING' || status !== 'IN_PROGRESS') : ((outcome === 'PENDING' && status !== 'HUNG_UP') || (outcome === 'ANSWERED' && status !== 'HUNG_UP'));
 
   return (
     <div id={htmlID}>
