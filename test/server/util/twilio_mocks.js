b21365989e512909851a89254825f2e1b8c9876a
Jadzia <jadzia@twilio.com>
2017-09-13 12:08:29 -0700

    Use twilio client to mutate call for connecting volunteers to contacts

--- test/server/util/twilio_mocks.js
+++ test/server/util/twilio_mocks.js
@@ -1,7 +1,10 @@
 const { TWILIO_ACCOUNT_SID } = process.env;
 
-export function callStatusUpdate() {
-  return {
+export const CALL_COMPLETE = 'CALL_COMPLETE';
+export const CONNECT_CONTACT_URL = 'CONNECT_CONTACT_URL';
+
+export function returnUpdate(updateType) {
+  const clientUpdateObject = {
     accountSid: `${TWILIO_ACCOUNT_SID}`,
     annotation: null,
     answeredBy: null,
@@ -10,8 +13,8 @@ export function callStatusUpdate() {
     dateCreated: '2017-09-11T18:20:15.000Z',
     dateUpdated: '2017-09-11T18:20:44.000Z',
     direction: 'outbound-api',
-    duration: '14',
-    endTime: '2017-09-11T18:20:44.000Z',
+    duration: null,
+    endTime: null,
     forwardedFrom: null,
     from: '+14083409940',
     fromFormatted: '(408) 340-9940',
@@ -22,7 +25,7 @@ export function callStatusUpdate() {
     priceUnit: 'USD',
     sid: 'CAcbbf06f666c72c51c59200de56ae54ff',
     startTime: '2017-09-11T18:20:30.000Z',
-    status: 'completed',
+    status: null,
     subresourceUris: {
       notifications: `/2010-04-01/Accounts/${TWILIO_ACCOUNT_SID}/Calls/CAcbbf06f666c72c51c59200de56ae54ff/Notifications.json`,
       recordings: `/2010-04-01/Accounts/${TWILIO_ACCOUNT_SID}/Calls/CAcbbf06f666c72c51c59200de56ae54ff/Recordings.json`
@@ -30,17 +33,62 @@ export function callStatusUpdate() {
     to: '+14086428264',
     toFormatted: '(408) 642-8264',
     uri: `/2010-04-01/Accounts/${TWILIO_ACCOUNT_SID}/Calls/CAcbbf06f666c72c51c59200de56ae54ff.json`
-  };
+  }
+  switch (updateType) {
+    case CALL_COMPLETE:
+      clientUpdateObject.duration = '14';
+      clientUpdateObject.endTime = '2017-09-11T18:20:44.000Z'
+      clientUpdateObject.status = 'completed'
+      return clientUpdateObject;
+    case CONNECT_CONTACT_URL:
+      clientUpdateObject.status = 'in-progress'
+      return clientUpdateObject;
+    default:
+      return clientUpdateObject;
+  }
 }
 
 export function createCall() {
   // Allie's create call callback Object goes here
 }
 
+//stole this array comparison extension from stackoverflow
+Array.prototype.equals = function (array) {
+    // if the other array is a falsy value, return
+    if (!array)
+        return false;
+    // compare lengths - can save a lot of time
+    if (this.length != array.length)
+        return false;
+
+    for (var i = 0, l=this.length; i < l; i++) {
+        // Check if we have nested arrays
+        if (this[i] instanceof Array && array[i] instanceof Array) {
+            // recurse into the nested arrays
+            if (!this[i].equals(array[i]))
+                return false;
+        }
+        else if (this[i] != array[i]) {
+            // Warning - two different object instances will never be equal: {x:20} != {x:20}
+            return false;
+        }
+    }
+    return true;
+}
+
 export const testTwilioClient = {
   calls(callSid) {
     return {
-      update: () => callStatusUpdate()
+      update: (options) => {
+        const optionsCalled = Object.getOwnPropertyNames(options).sort();
+        if (optionsCalled.equals([ 'status', 'statusCallback' ])) {
+          return returnUpdate(CALL_COMPLETE);
+        } else if (optionsCalled.equals([ 'url' ])) {
+          return returnUpdate(CONNECT_CONTACT_URL);
+        } else {
+          return returnUpdate();
+        }
+      }
     };
   }
-};
+}
