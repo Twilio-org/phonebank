dd2ce628f9d8a827c32872b57272a2afb9e7154b
Andrea Oneto <aoneto@twilio.com>
2017-09-14 13:58:10 -0700

    integrated hangup flow with the most updated code
    broke out hang up flow into a helper function and invoked it inside the controller

--- server/controllers/calls.js
+++ server/controllers/calls.js
@@ -108,6 +108,31 @@ function afterPutCallAttempt(res, outcome, contact_id, attempt_num, campaign_id)
   }
   return res.status(200).json({ message: 'call log successfully updated' });
 }
+
+function handleHangUpFlow(res, user_id, call_id, newStatus) {
+  return usersService.getUserById({ id: user_id })
+  .then((userObj) => {
+    const { call_sid: userCallSid } = userObj.attributes;
+    hangUpContactCall(userCallSid, user_id)
+    .then(() => {
+      callsService.updateCallStatus({ id: call_id, status: newStatus })
+      .then((updatedCall) => {
+        const updateCallSuccess = `call status updated to ${newStatus}`;
+        return res.status(200)
+          .json({ message: updateCallSuccess, call: updatedCall });
+      })
+      .catch((err) => {
+        const updateCallError = `Could not update call status to ${newStatus}: ${err}`;
+        return res.status(500).json({ message: updateCallError });
+      });
+    })
+    .catch((err) => {
+      const hangUpCallError = `Could not hang up call from twilio client to contact: ${err}`;
+      return res.status(500).json({ message: hangUpCallError });
+    });
+  })
+  .catch(err => err);
+}
 /* ======== END HELPERS ========== */
 
 export function recordAttempt(req, res) {
@@ -141,9 +166,7 @@ export function recordAttempt(req, res) {
             const { contact_id, campaign_id, status } = call.attributes;
             if (validateStatusForUpdate(newStatus, status)) {
               if (newStatus === 'HUNG_UP') {
-                return callsService.updateCallStatus({ id: call_id, status: newStatus })
-                  .then(updatedCall => res.status(200).json({ message: `call status updated to ${newStatus}`, call: updatedCall }))
-                  .catch(err => console.log('error updating status in calls controller: ', err));
+                return handleHangUpFlow(res, user_id, call_id, newStatus);
               } else if (newStatus === 'IN_PROGRESS') {
                 return usersService.getUserById({ id: user_id })
                   .then((user) => {
