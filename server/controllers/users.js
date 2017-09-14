8b8d224292a8b4e068a93cb4d6d69761cd2833bc
Aiden Nguyen <nguyenaiden@gmail.com>
2017-09-14 13:19:41 -0700

    [DOTORG-121] Handle Twilio callbacks for contact calls (#152)
    * Add route in user's routes to handle callback
    * Expand user's controller to handle callback
    * Expand calls service to update call_sid
    * Add calls service test for updateContactCallSid

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,5 +1,6 @@
 import usersService from '../db/services/users';
 import campaignsService from '../db/services/campaigns';
+import callsService from '../db/services/calls';
 import { callVolunteer, sayCallCompleted, sayHelloUser } from '../util/twilio';
 
 
@@ -273,6 +274,22 @@ export function volunteerCallback(req, res) {
     })
     .catch((err) => {
       res.status(500).json({ message: `Could not process request to clear user Call SID on volunteer callback: ${err}` });
+    });
+}
+
+export function contactCallback(req, res) {
+  const { call_id: id } = req.params;
+  const { call_sid } = req.body;
+  return callsService.updateContactCallSid({ id, call_sid })
+    .then((call) => {
+      if (call) {
+        res.status(201).json(call);
+      } else {
+        res.status(404).json({ message: 'Could not find call with given call id' });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ message: `Could not save Call SID for this contact for this call: ${err}` });
     });
 }
 
