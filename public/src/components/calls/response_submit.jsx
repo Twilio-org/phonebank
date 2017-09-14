60069d7a8fe8562766f9317024822150e03fef97
Lupita Davila <lupita.dav@gmail.com>
2017-09-14 14:23:41 -0700

    remove unused file

--- public/src/components/calls/response_submit.jsx
+++
@@ -1,9 +0,0 @@
-import { submitCallResponses } from '../../actions/calls';
-
-export default function formSubmission(dispatchInstance) {
-  const dispatch = dispatchInstance;
-  return function responseSubmit(values) {
-    console.log('THISSSS IS THE FORM SUBMIT =================================', values);
-    return dispatch(submitCallResponses(values));
-  };
-}
