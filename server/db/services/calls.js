d417ab7ee12460f38b24e9401f5a3a3d65019a1e
Jadzia <jadzia@twilio.com>
2017-09-15 14:38:55 -0700

    Make call assignment service assign previously assigned calls.

--- server/db/services/calls.js
+++ server/db/services/calls.js
@@ -5,19 +5,27 @@ export default {
     const { user_id, campaign_id } = params;
 
     return new Call()
-      .where({ campaign_id, status: 'AVAILABLE' })
-      .orderBy('id', 'ASC')
+      .where({ campaign_id, user_id, status: 'ASSIGNED' })
       .fetch()
-      .then((call) => {
-        if (call) {
-          return call.save({ user_id, status: 'ASSIGNED' }, { patch: true })
-            .then(savedCall => savedCall)
-            .catch(err => console.log('Error in call service when assigning to user: ', err));
-        }
+      .then((previousCall) => {
+        if (!previousCall) {
+          return new Call()
+            .where({ campaign_id, status: 'AVAILABLE' })
+            .orderBy('id', 'ASC')
+            .fetch()
+            .then((call) => {
+              if (call) {
+                return call.save({ user_id, status: 'ASSIGNED' }, { patch: true })
+                  .then(savedCall => savedCall)
+                  .catch(err => console.log('Error in call service when assigning to user: ', err));
+              }
 
-        return null;
-      })
-      .catch(err => console.log('Error in call service when finding call to assign:', err));
+              return null;
+            })
+            .catch(err => console.log('Error in call service when finding call to assign:', err));
+        }
+        return previousCall;
+      });
   },
 
   createNewAttempt: (params) => {
