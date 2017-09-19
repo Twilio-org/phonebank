314775963273a7a6a06c0b4b31e299b0e39cb15e
Andrea Oneto <aoneto@twilio.com>
2017-09-19 15:03:35 -0700

    done with response testing next is campaigns testing

--- test/server/services/responses.js
+++ test/server/services/responses.js
@@ -33,13 +33,29 @@ describe('Responses Service tests', () => {
         description: 'Script Description 7'
       };
 
-      this.contactSaveParams = {
-        first_name: 'Katie',
-        last_name: 'Smith',
-        phone_number: '+11820020000',
-        email: 'katie@gmail.com',
-        external_id: 'test12345'
-      };
+      this.contactSaveParams = [
+        {
+          first_name: 'Katie',
+          last_name: 'Smith',
+          phone_number: '+11820020000',
+          email: 'katie@gmail.com',
+          external_id: 'test12345'
+        },
+        {
+          first_name: 'Susan',
+          last_name: 'Mau',
+          phone_number: '+11820024444',
+          email: 'katie@gmail.com',
+          external_id: 'test12345'
+        },
+        {
+          first_name: 'Aaron',
+          last_name: 'Smith',
+          phone_number: '+11820026666',
+          email: 'katie@gmail.com',
+          external_id: 'test12345'
+        }
+      ];
 
       this.userSaveParams = {
         firstName: 'John',
@@ -51,9 +67,17 @@ describe('Responses Service tests', () => {
 
       this.callSaveParams = {};
 
-      this.responseParams = {
-        response: 'Yes'
-      };
+      this.responseParams = [
+        {
+          response: 'Yes'
+        },
+        {
+          response: 'No'
+        },
+        {
+          response: 'Yes'
+        }
+      ];
 
       this.questionParams = {
         title: 'Sloths',
@@ -70,61 +94,99 @@ describe('Responses Service tests', () => {
               this.campaignParams.contact_lists_id = contactList.attributes.id;
               campaignsService.saveNewCampaign(this.campaignParams)
                 .then((campaign) => {
-                  this.callSaveParams.campaign_id = campaign.attributes.id;
-                  contactsService.saveNewContact(this.contactSaveParams)
-                    .then((contact) => {
-                      this.callSaveParams.contact_id = contact.attributes.id;
-                      usersService.saveNewUser(this.userSaveParams)
+                  this.callSaveParams = [];
+                  for (let i = 0; i < 3; i += 1) {
+                    this.callSaveParams.push({ campaign_id: campaign.attributes.id });
+                  }
+                  Promise.all(this.contactSaveParams.map((contact, index) => {
+                    return contactsService.saveNewContact(contact)
+                      .then((newContact) => {
+                        this.callSaveParams[index].contact_id = newContact.attributes.id;
+                      });
+                  }))
+                  .then(() => {
+                    usersService.saveNewUser(this.userSaveParams)
                       .then((user) => {
                         this.callSaveParams.user_id = user.attributes.id;
-                        callsService.populateCall(this.callSaveParams)
-                          .then((call) => {
-                            this.responseParams.call_id = call.attributes.id;
+                        Promise.all(this.callSaveParams.map((call, index) => {
+                          return callsService.populateCall(call)
+                            .then((populatedCall) => {
+                              this.responseParams[index].call_id = populatedCall.attributes.id;
+                            });
+                        }))
+                          .then(() => {
                             questionsService.saveNewQuestion(this.questionParams)
                               .then((question) => {
-                                this.responseParams.question_id = question.attributes.id;
+                                this.responseParams.forEach((response) => {
+                                  response.question_id = question.attributes.id;
+                                });
                                 done();
                               });
                           });
                       });
-                    });
+                  });
                 });
             });
         });
     });
 
-
     it('should save a new response to with question_id 1 and call_id 4', (done) => {
-      responsesService.saveNewResponse(this.responseParams)
+      responsesService.saveNewResponse(this.responseParams[0])
         .then((response) => {
+          this.responseParams[0].id = response.attributes.id;
           expect(response.attributes.id).to.equal(1);
-          expect(response.attributes.question_id).to.equal(this.responseParams.question_id);
-          expect(response.attributes.call_id).to.equal(this.responseParams.call_id);
-          expect(response.attributes.response).to.equal(this.responseParams.response);
+          expect(response.attributes.question_id).to.equal(this.responseParams[0].question_id);
+          expect(response.attributes.call_id).to.equal(this.responseParams[0].call_id);
+          expect(response.attributes.response).to.equal(this.responseParams[0].response);
           done();
         }, done);
     });
 
     it('should fetch responses with a specific question id: ', (done) => {
-      // fetchResponsesByQuestionId
-      // return responsesService.saveNewResponse(this.responseParams)
-      //   .then((newResponse) => {
-      //     if (newResponse) {
-      const { question_id } = this.responseParams;
-      return responsesService.fetchResponsesByQuestionId({ question_id })
-      .then((responseModels) => {
-        // should be only one response
-        const { question_id: questionId, call_id, response } = responseModels[0].attributes;
-        expect(response.attributes.id).to.equal(1);
-        expect(response.attributes.question_id).to.equal(questionId);
-        expect(response.attributes.call_id).to.equal(call_id);
-        expect(response.attributes.response).to.equal(response);
-        done();
-      }, done)
-      .catch(err => console.log(`error with fetchResponsesByQuestionId test: ${err}`));
-        //   }
-        // }, done)
-        // .catch(err => console.log(`error saving new response in response tests: ${err}`));
+      const [first, second, third] = this.responseParams;
+      // return createTwoNewResponses(second, third, done)
+      responsesService.saveNewResponse(second)
+        .then((secondResponseModel) => {
+          this.responseParams[1].id = secondResponseModel.attributes.id;
+          responsesService.saveNewResponse(third)
+            .then((thirdResponseModel) => {
+              this.responseParams[2].id = thirdResponseModel.attributes.id;
+              const { question_id } = this.responseParams[0];
+              responsesService.fetchResponsesByQuestionId({ question_id })
+              .then((responses) => {
+                const { length, models: responseModels } = responses;
+                const [firstResponse, secondResponse, thirdResponse] = responseModels;
+                const { id: idOne,
+                        question_id: questionIdOne,
+                        call_id: callIdOne,
+                        response: responseOne } = firstResponse.attributes;
+                const { id: idTwo,
+                        question_id: questionIdTwo,
+                        call_id: callIdTwo,
+                        response: responseTwo } = secondResponse.attributes;
+                const { id: idThree,
+                        question_id: questionIdThree,
+                        call_id: callIdThree,
+                        response: responseThree } = thirdResponse.attributes;
+                expect(length).to.equal(this.responseParams.length);
+                expect(idOne).to.equal(this.responseParams[0].id);
+                expect(questionIdOne).to.equal(first.question_id);
+                expect(callIdOne).to.equal(first.call_id);
+                expect(responseOne).to.equal(first.response);
+                expect(idTwo).to.equal(second.id);
+                expect(questionIdTwo).to.equal(question_id);
+                expect(callIdTwo).to.equal(second.call_id);
+                expect(responseTwo).to.equal(second.response);
+                expect(idThree).to.equal(third.id);
+                expect(questionIdThree).to.equal(question_id);
+                expect(callIdThree).to.equal(third.call_id);
+                expect(responseThree).to.equal(third.response);
+                expect(questionIdOne).to.equal(questionIdThree);
+                expect(questionIdTwo).to.equal(questionIdThree);
+                done();
+              }, done);
+            }, done);
+        }, done);
     });
   });
 });
