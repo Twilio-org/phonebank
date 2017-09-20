f6a37066d58bc8f7c7497f32edc3989e06494a6a
Allie Glotfelty <aglotfelty@twilio.com>
2017-09-20 09:58:23 -0700

    Remove console log from markCampaignCompleted

--- server/db/services/campaigns.js
+++ server/db/services/campaigns.js
@@ -28,7 +28,6 @@ export default {
 
   markCampaignAsCompleted: (params) => {
     const { id } = params;
-    console.log('in markCampaignAsCompleted service!!!');
     return new Campaign()
       .where({ id })
       .save({
