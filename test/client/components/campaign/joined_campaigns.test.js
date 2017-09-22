7cad8a0a5a811708322ed21ec6fba583decb8f58
Allie Glotfelty <aglotfelty@twilio.com>
2017-09-22 13:34:41 -0700

    Delete console.log

--- test/client/components/campaign/joined_campaigns.test.js
+++ test/client/components/campaign/joined_campaigns.test.js
@@ -41,7 +41,6 @@ describe('<JoinedCampaigns />', () => {
     });
     it('Should call the fetchCampaignsByUser action on mount', () => {
       mount(<JoinedCampaigns {...props} />);
-      console.log('id in joined campaigns test is: ', props.id);
       expect(props.fetchCampaignsByUser).toHaveBeenCalled();
     });
   });
