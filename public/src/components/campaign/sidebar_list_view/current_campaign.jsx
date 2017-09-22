4f243f74f11569f4a9d0c8e92a9940419e24f554
Allie Glotfelty <aglotfelty@twilio.com>
2017-09-21 22:50:51 -0700

    Add OverlayTrigger and popover to Start Calling button

--- public/src/components/campaign/sidebar_list_view/current_campaign.jsx
+++ public/src/components/campaign/sidebar_list_view/current_campaign.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, OverlayTrigger, Tooltip, Popover } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import _ from 'lodash';
 
@@ -23,14 +23,27 @@ const CurrentCampaign = (props) => {
     history.push(`/volunteers/campaigns/${campaignId}/calls`);
   };
   const { description, status, title, id: campaignId } = currentCampaign;
+  // const tooltip = (
+  //   <Tooltip id="tooltip">Once you click the <strong>Start Calling</strong> button, the phonebank will initialize a call to the phone number you registered with.</Tooltip>
+  // );
+
+  const popover = (
+    <Popover id="popover-trigger-hover">
+      Once you click the <strong>Start Calling</strong> button, the phonebank will initialize a call to the phone number you registered with.
+    </Popover>
+  );
+
+
   return (
     <div id={campaignId}>
       <h3>{title}</h3>
       <hr />
       <p className="lead">{description}</p>
-      <Button onClick={handleClick} bsStyle="primary" disabled={status !== 'active'}>
-        <i className="material-icons small">phone_in_talk</i> Start Calling
-      </Button>
+      <OverlayTrigger trigger="hover" placement="right" overlay={popover}>
+        <Button onClick={handleClick} bsStyle="primary" disabled={status !== 'active'}>
+          <i className="material-icons small">phone_in_talk</i> Start Calling
+        </Button>
+      </OverlayTrigger>
     </div>
   );
 };
