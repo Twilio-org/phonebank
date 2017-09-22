4c1504e918729d6207c37a561e71bec0cfaf9cda
Allie Glotfelty <aglotfelty@twilio.com>
2017-09-22 13:41:10 -0700

    Remove Link import

--- public/src/components/campaign/sidebar_list_view/current_campaign.jsx
+++ public/src/components/campaign/sidebar_list_view/current_campaign.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Button, OverlayTrigger, Popover } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
 import _ from 'lodash';
 
 const CurrentCampaign = (props) => {
