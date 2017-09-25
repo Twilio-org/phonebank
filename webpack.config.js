f1608141d6bf22968fe37555bd99645eb87d7012
Josh Levinger <josh@levinger.net>
2017-09-25 14:11:32 -0700

    Add babel lodash plugin to prune tree

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,7 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
+          plugins: ['lodash'],
           presets: ['react', 'es2015', 'stage-1', 'env']
         }
       },
