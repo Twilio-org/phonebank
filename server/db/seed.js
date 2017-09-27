6bff81ee990f08af15caf185623ace523dc3925a
Jadzia <jadzia@twilio.com>
2017-09-27 14:53:11 -0700

    Demo seed and refresh command.

--- server/db/seed.js
+++ server/db/seed.js
@@ -1,12 +1,4 @@
 import faker from 'faker';
-import db from './db';
-import campaignsService from '../db/services/campaigns';
-import callsService from '../db/services/calls';
-import contactListsService from '../db/services/contact_lists';
-import contactsService from '../db/services/contacts';
-import questionsService from '../db/services/questions';
-import scriptsService from '../db/services/scripts';
-import usersService from '../db/services/users';
 import User from './models/users';
 
 function getRandomFourDigitInt() {
@@ -24,105 +16,16 @@ const userParams = [
     is_admin: true
   },
   {
-    first_name: faker.name.firstName(),
-    last_name: faker.name.firstName(),
-    email: 'user@foobar.com',
-    phone_number: `555-555-${getRandomFourDigitInt()}`,
+    first_name: 'Jadzia',
+    last_name: 'Thomas',
+    email: 'jadzia@twilio.com',
+    phone_number: '+14237712171',
     password_hash: password
   }
 ];
-const questionParams = [
-  {
-    title: 'Question 1',
-    description: 'test question 1',
-    type: 'singleselect',
-    responses: 'resp1,resp2,resp3,resp4,resp5'
-  },
-  {
-    title: 'Question 2',
-    description: 'test question 2',
-    type: 'paragraph',
-    responses: ''
-  },
-  {
-    title: 'Question 3',
-    description: 'test question 3',
-    type: 'multiselect',
-    responses: 'resp1,resp2,resp3,resp4,resp5'
-  }
-];
-const scriptParams = {
-  name: 'Script 1',
-  description: 'This is the first script.',
-  body: 'Script 1 body'
-};
-
-const contactParams = new Array(50).fill(null).map(() => {
-  const phone_number = `555-555-${getRandomFourDigitInt()}`;
-  const first_name = faker.name.firstName();
-  return {
-    first_name,
-    phone_number
-  };
-});
-
-const fullContactFirstName = faker.name.firstName();
-const fullContactLastName = faker.name.lastName();
-const fullContactEmail = `${fullContactFirstName}.${fullContactLastName}@notrealemail.com`;
-
-contactParams.push({
-  first_name: fullContactFirstName,
-  last_name: fullContactLastName,
-  phone_number: `555-555-${getRandomFourDigitInt()}`,
-  email: fullContactEmail,
-  external_id: 'external_id'
-});
-
-const contactListParams = {
-  name: 'Contact List 1'
-};
-let campaignScriptId;
-
-const callParams = [];
 
 function createUser(params) {
-  return new User(params).save()
-  .then().catch(err => console.log(err));
-}
-
-function createQuestion(params) {
-  return questionsService.saveNewQuestion(params)
-    .then().catch(err => console.log(err));
-}
-
-function createScriptQuestion(params) {
-  return scriptsService.addQuestionToScript(params)
-    .then().catch(err => console.log(err));
-}
-
-function createContact(params) {
-  return contactsService.saveNewContact(params)
-    .then().catch(err => console.log(err));
-}
-
-function createContactListContact(params) {
-  return contactListsService.addContactToContactList(params)
-  .then().catch(err => console.log(err));
-}
-
-function createCall(params) {
-  return callsService.populateCall(params)
-  .then().catch(err => console.log(err));
-}
-
-function updateContactDoNotCall(params) {
-  return contactsService.updateContactDoNotCallById(params)
-  .then().catch(err => console.log(err));
-}
-
-function updateContactInvalidNumber(params) {
-  return contactsService.updateContactInvalidNumberById(params)
-  .then().catch(err => console.log(err));
+  return new User(params).save().then().catch(err => console.log(err));
 }
 
 function generatePromiseActions(paramsArray, createFunction) {
@@ -136,78 +39,4 @@ function generatePromiseActions(paramsArray, createFunction) {
 }
 
 Promise.all(generatePromiseActions(userParams, createUser))
-  .then((users) => {
-    Promise.all(generatePromiseActions(questionParams, createQuestion))
-      .then((questions) => {
-        scriptsService.saveNewScript(scriptParams)
-          .then((script) => {
-            campaignScriptId = script.attributes.id;
-            const id = campaignScriptId;
-            const questionIds = questions.map(question => question.attributes.id);
-            const scriptQuestionParams = [];
-            questionIds.forEach((questionId, index) => {
-              scriptQuestionParams.push({
-                id,
-                question_id: questionId,
-                sequence_number: index + 1
-              });
-            });
-
-            Promise.all(generatePromiseActions(scriptQuestionParams, createScriptQuestion))
-              .then(() => {
-                contactListsService.saveNewContactList(contactListParams)
-                  .then((contactList) => {
-                    Promise.all(generatePromiseActions(contactParams, createContact))
-                      .then((contacts) => {
-                        const contactIds = contacts.map(contact => contact.attributes.id);
-                        const contactListContactParams = [];
-
-                        contactIds.forEach((contactId) => {
-                          if (contactId > 30 && contactId < 40) {
-                            updateContactDoNotCall({ id: contactId });
-                          } else if (contactId > 40 && contactId < 50) {
-                            updateContactInvalidNumber({ id: contactId });
-                          }
-                          contactListContactParams.push({
-                            id: contactList.attributes.id,
-                            contact_id: contactId
-                          });
-                          callParams.push({ contact_id: contactId });
-                        });
-
-                        Promise.all(generatePromiseActions(contactListContactParams,
-                                                           createContactListContact))
-                          .then(() => {
-                            const campaignParams = {
-                              name: 'Campaign 1',
-                              title: 'Campaign 1 Title',
-                              description: 'Campaign 1 description',
-                              status: 'active',
-                              contact_lists_id: contactList.attributes.id,
-                              script_id: campaignScriptId
-                            };
-
-                            campaignsService.saveNewCampaign(campaignParams)
-                              .then((campaign) => {
-                                const campaignId = campaign.attributes.id;
-                                const userId = users.map(user => user.attributes.id)[1];
-                                const cloneCallParams = callParams.map(call =>
-                                  Object.assign({}, call, { campaign_id: campaignId }));
-
-                                Promise.all(generatePromiseActions(cloneCallParams, createCall))
-                                  .then(() => {
-                                    usersService.addCampaignToUser({
-                                      campaign_id: campaignId,
-                                      id: userId
-                                    }).then(() => {
-                                      db.knex.destroy(() => console.log('All Entries created.'));
-                                    }).catch(err => console.log(err));
-                                  }).catch(err => console.log(err));
-                              }).catch(err => console.log(err));
-                          }).catch(err => console.log(err));
-                      }).catch(err => console.log(err));
-                  }).catch(err => console.log(err));
-              }).catch(err => console.log(err));
-          }).catch(err => console.log(err));
-      }).catch(err => console.log(err));
-  });
+  .then().catch(err => console.log(err));
