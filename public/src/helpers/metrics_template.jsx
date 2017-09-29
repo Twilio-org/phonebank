17ba3268605b8011adaad5362cbd90746420aa7c
Andrea Oneto <aoneto@twilio.com>
2017-09-28 23:04:54 -0700

    removed unnecessary console.log statements

--- public/src/helpers/metrics_template.jsx
+++ public/src/helpers/metrics_template.jsx
@@ -97,10 +97,8 @@ function formatResponse(rawResponse) {
 export function getQuestionNames(questions) {
   const questionNames = {};
   questions.forEach((question) => {
-    console.log('question: ', question)
     questionNames[question.id] = question.title;
   });
-  console.log('questionNAMES: ', questionNames);
   return questionNames;
 }
 
