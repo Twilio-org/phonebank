ec8f09d833fb6eee43edaa73552d1d26a2fae1be
Josh Levinger <josh@levinger.net>
2017-10-03 17:04:20 -0700

    Index file location bug

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,7 +2,7 @@ import path from 'path';
 
 export default function serveReactApp(req, res) {
   if (process.env.NODE_ENV === 'production') {
-    res.sendFile(path.resolve(__dirname, '../../public/dist/index.html'));
+    res.sendFile(path.resolve(__dirname, '../../public/dist/src/index.html'));
   } else {
     res.render(path.resolve(__dirname, '../../public/index.html'));
   }
